856fdf0ee3ac38d47caab88650f7dc691c1f7bb7
mgoulish <mgoulish@redhat.com>
2020-06-11 12:28:25 -0400

    Add service informer.

--- client/van_serviceinterface_create_test.go
+++ client/van_serviceinterface_create_test.go
@@ -13,12 +13,10 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/tools/cache"
-	rbacv1 "k8s.io/api/rbac/v1"
 )
-
-var fp = fmt.Fprintf
 
 // If this function detects a difference between the expected and observed
 // results, it asserts a test failure.
@@ -27,11 +25,11 @@ func check_result(t *testing.T, timeout_seconds float64, expected []string, foun
 		return
 	}
 	for {
-                // Sometimes it requires a little time for the requested entities to be
-                // created and for the informers to tell us about them. 
-                // So -- count down by tenths of a second until the alotted timeout expires, 
-                // or until we have the correct number of results. 
-                // If the latter -- check to make sure that they are as expected.
+		// Sometimes it requires a little time for the requested entities to be
+		// created and for the informers to tell us about them.
+		// So -- count down by tenths of a second until the alotted timeout expires,
+		// or until we have the correct number of results.
+		// If the latter -- check to make sure that they are as expected.
 		if timeout_seconds <= 0 {
 			assert.Assert(t, false, fmt.Sprintf("%s: timed out waiting for expected list size %d.\n", doc, len(expected)))
 			break
@@ -61,6 +59,7 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 		depsExpected  []string
 		cmsExpected   []string
 		rolesExpected []string
+		svcsExpected  []string
 		timeout       float64 // seconds
 	}{
 		// The first four tests look at error returns (or the lack thereof)
@@ -102,23 +101,29 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 			addr:  "half-addr",
 			proto: "tcp",
 			port:  314159,
-			err:   "error", 
+			err:   "error",
 		},
 
 		// The remaining tests verify the expected side-effects
 		// of VAN service interface creation.
 		// I.e., certain deployments and services should be created.
+
+                // NOTE: about the mock client.
+                //       Once we have the ability to run these unit tests 
+                //       against a real cluster, the 'expected' lists below
+                //       will change.
 		{
 			name:          "vsic_5",
 			doc:           "Check basic deployments.",
 			init:          true,
 			addr:          "half-addr",
 			proto:         "tcp",
-			port:          1776,
+			port:          1999,
 			err:           "",
 			depsExpected:  []string{"skupper-router", "skupper-service-controller"},
 			cmsExpected:   []string{"skupper-services"},
 			rolesExpected: []string{"skupper-edit", "skupper-view"},
+			svcsExpected:  []string{"skupper-messaging", "skupper-internal", "skupper-controller"},
 			timeout:       5.0,
 		},
 	}
@@ -130,6 +135,7 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 		depsFound := []string{}
 		cmsFound := []string{}
 		rolesFound := []string{}
+		svcsFound := []string{}
 
 		cli, err := newMockClient("skupper", "", "")
 		assert.Check(t, err, testcase.name)
@@ -138,44 +144,55 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 		// Create all informers that will let us check the expected
 		// side effects of starting the VAN Service Interface..
 		//------------------------------------------------------------
-		var informer_list []cache.SharedIndexInformer
+		var informerList []cache.SharedIndexInformer
 
 		// Deployment Informer -------------------------
-		informer_factory := informers.NewSharedInformerFactory(cli.KubeClient, 0)
-		depInformer := informer_factory.Apps().V1().Deployments().Informer()
+		informerFactory := informers.NewSharedInformerFactory(cli.KubeClient, 0)
+		depInformer := informerFactory.Apps().V1().Deployments().Informer()
 		depInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
 				dep := obj.(*appsv1.Deployment)
 				depsFound = append(depsFound, dep.Name)
 			},
 		})
-		informer_list = append(informer_list, depInformer)
+		informerList = append(informerList, depInformer)
 
 		// Config Map Informer -------------------------
-		cmInformer := informer_factory.Core().V1().ConfigMaps().Informer()
+		cmInformer := informerFactory.Core().V1().ConfigMaps().Informer()
 		cmInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
 				cm := obj.(*corev1.ConfigMap)
 				cmsFound = append(cmsFound, cm.Name)
 			},
 		})
-		informer_list = append(informer_list, cmInformer)
+		informerList = append(informerList, cmInformer)
 
 		// Role Informer -------------------------
-		roleInformer := informer_factory.Rbac().V1().Roles().Informer()
+		roleInformer := informerFactory.Rbac().V1().Roles().Informer()
 		roleInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
 				role := obj.(*rbacv1.Role)
 				rolesFound = append(rolesFound, role.Name)
 			},
 		})
-		informer_list = append(informer_list, roleInformer)
+		informerList = append(informerList, roleInformer)
+
+		// Service Informer -------------------------
+		svcInformer := informerFactory.Core().V1().Services().Informer()
+		svcInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				svc := obj.(*corev1.Service)
+				svcsFound = append(svcsFound, svc.Name)
+			},
+		})
+
+		informerList = append(informerList, svcInformer)
 
 		//------------------------------------------------------------
 		// Start all the informers and wait until each one is ready.
 		//------------------------------------------------------------
-		informer_factory.Start(ctx.Done())
-		for _, i := range informer_list {
+		informerFactory.Start(ctx.Done())
+		for _, i := range informerList {
 			result := cache.WaitForCacheSync(ctx.Done(), i.HasSynced)
 			assert.Check(t, result, "Unable to sync an informer.")
 		}
@@ -218,5 +235,6 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 		check_result(t, testcase.timeout, testcase.depsExpected, &depsFound, testcase.doc)
 		check_result(t, testcase.timeout, testcase.cmsExpected, &cmsFound, testcase.doc)
 		check_result(t, testcase.timeout, testcase.rolesExpected, &rolesFound, testcase.doc)
+		check_result(t, testcase.timeout, testcase.svcsExpected, &svcsFound, testcase.doc)
 	}
 }
