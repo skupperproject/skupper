5af819f96c316b4331b36095516584ad073026c5
nicob87 <nmbrignone@gmail.com>
2020-06-16 09:37:37 -0300

    VanConnectorCreate cluster test (#49)
    Running client package unit-test also against a real cluster

--- client/client_test.go
+++ client/client_test.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"flag"
+	"os"
 	"testing"
 
 	"gotest.tools/assert"
@@ -28,4 +30,11 @@ func TestNewClient(t *testing.T) {
 		_, err := newMockClient(c.namespace, c.context, c.kubeConfigPath)
 		assert.Check(t, err, c.doc)
 	}
+}
+
+var clusterRun = flag.Bool("use-cluster", false, "run tests against a configured cluster")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	os.Exit(m.Run())
 }
