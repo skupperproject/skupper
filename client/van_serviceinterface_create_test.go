a3ac72a5a7345da35010c442fe521e02b56f6063
Michael Goulish <mick@redhat.com>
2020-07-18 18:49:12 -0400

    VAN Service Interface test now works with a real cluster as well as in (#111)
    * VAN Service Interface test now works with a real cluster as well as in
    the mock environment.
    
    * One extra service is created when using real cluster.
    
    Co-authored-by: mgoulish <mgoulish@redhat.com>

--- client/van_serviceinterface_create_test.go
+++ client/van_serviceinterface_create_test.go
@@ -2,11 +2,14 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/skupperproject/skupper/api/types"
+	"github.com/skupperproject/skupper/pkg/kube"
 	"gotest.tools/assert"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -16,9 +19,11 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
+var fp = fmt.Fprintf
+
 // If this function detects a difference between the expected and observed
 // results, it asserts a test failure.
-func check_result(t *testing.T, timeoutSeconds float64, resultType string, expected []string, found *[]string, doc string) {
+func check_result(t *testing.T, name string, timeoutSeconds float64, resultType string, expected []string, found *[]string, doc string) {
 	if len(expected) <= 0 {
 		return
 	}
@@ -33,29 +38,30 @@ func check_result(t *testing.T, timeoutSeconds float64, resultType string, expec
 		if len(*found) >= len(expected) {
 			break
 		}
-		time.Sleep(100 * time.Millisecond)
-		timeoutSeconds -= 0.1
+		time.Sleep(time.Second)
+		timeoutSeconds -= 1.0
 	}
 	if diff := cmp.Diff(expected, *found, trans); diff != "" {
-		t.Errorf("TestVanServiceInterfaceCreate %s mismatch (-want +got):\n%s", resultType, diff)
+		t.Errorf("TestVanServiceInterfaceCreate %s : %s mismatch (-want +got):\n%s", name, resultType, diff)
 	}
 }
 
 func TestVanServiceInterfaceCreate(t *testing.T) {
 	testcases := []struct {
-		name          string
-		doc           string
-		init          bool
-		err           string
-		addr          string
-		proto         string
-		port          int
-		user          string
-		depsExpected  []string
-		cmsExpected   []string
-		rolesExpected []string
-		svcsExpected  []string
-		timeout       float64 // seconds
+		namespace        string
+		doc              string
+		init             bool
+		expectedErr      string
+		addr             string
+		proto            string
+		port             int
+		user             string
+		depsExpected     []string
+		cmsExpected      []string
+		rolesExpected    []string
+		svcsExpected     []string
+		realSvcsExpected []string
+		timeout          float64 // seconds
 	}{
 		// The first four tests look at error returns (or the lack thereof)
 		// caused by bad (or not bad) arguments. An expected error of "" means
@@ -63,63 +69,65 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 		// means that some kind of error should be returned. Anything else
 		// means that the exact given error should be returned.
 		{
-			name:  "vsic_1",
-			doc:   "Uninitialized.",
-			init:  false,
-			addr:  "",
-			proto: "",
-			port:  0,
-			err:   "Skupper not initialised in skupper",
+			namespace:   "vsic-1",
+			doc:         "Uninitialized.",
+			init:        false,
+			addr:        "",
+			proto:       "",
+			port:        0,
+			expectedErr: "Skupper not initialised",
 		},
 		{
-			name:  "vsic_2",
-			doc:   "Normal initialization.",
-			init:  true,
-			addr:  "half-addr",
-			proto: "tcp",
-			port:  5672,
-			err:   "",
+			namespace:   "vsic-2",
+			doc:         "Normal initialization.",
+			init:        true,
+			addr:        "vsic-2-addr",
+			proto:       "tcp",
+			port:        5672,
+			expectedErr: "",
 		},
 		{
-			name:  "vsic_3",
-			doc:   "Bad protocol.",
-			init:  true,
-			addr:  "half-addr",
-			proto: "BISYNC",
-			port:  64000,
-			err:   "BISYNC is not a valid mapping. Choose 'tcp', 'http' or 'http2'.",
+			namespace:   "vsic-3",
+			doc:         "Bad protocol.",
+			init:        true,
+			addr:        "vsic-3-addr",
+			proto:       "BISYNC",
+			port:        64000,
+			expectedErr: "BISYNC is not a valid mapping",
 		},
 		{
-			name:  "vsic_4",
-			doc:   "Bad port.",
-			init:  true,
-			addr:  "half-addr",
-			proto: "tcp",
-			port:  314159,
-			err:   "error",
+			namespace:   "vsic-4",
+			doc:         "Bad port.",
+			init:        true,
+			addr:        "vsic-4-addr",
+			proto:       "tcp",
+			port:        314159,
+			expectedErr: "outside valid range",
 		},
 
 		// The remaining tests verify the expected side-effects
 		// of VAN service interface creation.
 		// I.e., certain deployments and services should be created.
-
-		// NOTE: about the mock client.
-		//       Once we have the ability to run these unit tests
-		//       against a real cluster, the 'expected' lists below
-		//       will change.
 		{
-			name:          "vsic_5",
+			namespace:     "vsic-5",
 			doc:           "Check basic deployments.",
 			init:          true,
-			addr:          "half-addr",
+			addr:          "vsic-5-addr",
 			proto:         "tcp",
 			port:          1999,
-			err:           "",
+			expectedErr:   "",
 			depsExpected:  []string{"skupper-router", "skupper-service-controller"},
 			cmsExpected:   []string{"skupper-internal", "skupper-services"},
 			rolesExpected: []string{"skupper-edit", "skupper-view"},
-			svcsExpected:  []string{"skupper-messaging", "skupper-internal", "skupper-controller"},
-			timeout:       5.0,
+			// The list of expected services is slightly different in
+			// the mock environment vs. a real cluster.
+			// It usually takes 10 or 12 seconds for the address service to
+			// show up, but I am giving it a large timeout here. The result
+			// checker will cut out as soon as it sees a result list of the
+			// right size.
+			svcsExpected:     []string{"skupper-messaging", "skupper-internal", "skupper-controller"},
+			realSvcsExpected: []string{"skupper-messaging", "skupper-internal", "skupper-controller", "vsic-5-addr"},
+			timeout:          60.0,
 		},
 	}
 
@@ -132,8 +140,19 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 		rolesFound := []string{}
 		svcsFound := []string{}
 
-		cli, err := newMockClient("skupper", "", "")
-		assert.Check(t, err, testcase.name)
+		var cli *VanClient
+		var err error
+
+		isCluster := *clusterRun
+		if isCluster {
+			cli, err = NewClient(testcase.namespace, "", "")
+		} else {
+			cli, err = newMockClient(testcase.namespace, "", "")
+		}
+		assert.Check(t, err, testcase.namespace)
+		_, err = kube.NewNamespace(testcase.namespace, cli.KubeClient)
+		assert.Check(t, err, testcase.namespace)
+		defer kube.DeleteNamespace(testcase.namespace, cli.KubeClient)
 
 		//------------------------------------------------------------
 		// Create all informers that will let us check the expected
@@ -142,7 +161,8 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 		var informerList []cache.SharedIndexInformer
 
 		// Deployment Informer -------------------------
-		informerFactory := informers.NewSharedInformerFactory(cli.KubeClient, 0)
+		informerFactory := informers.NewSharedInformerFactoryWithOptions(cli.KubeClient, 0, informers.WithNamespace(testcase.namespace))
+
 		depInformer := informerFactory.Apps().V1().Deployments().Informer()
 		depInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
@@ -193,20 +213,22 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 		}
 
 		// Create a router.
-		err = cli.VanRouterCreate(ctx, types.VanSiteConfig{
-			Spec: types.VanSiteConfigSpec{
-				SkupperName:       "skupper",
-				IsEdge:            false,
-				EnableController:  true,
-				EnableServiceSync: true,
-				EnableConsole:     false,
-				AuthMode:          "",
-				User:              "",
-				Password:          "",
-				ClusterLocal:      true,
-			},
-		})
-		assert.Check(t, err, "Unable to create VAN router")
+		if testcase.init {
+			err = cli.VanRouterCreate(ctx, types.VanSiteConfig{
+				Spec: types.VanSiteConfigSpec{
+					SkupperName:       testcase.namespace,
+					IsEdge:            false,
+					EnableController:  true,
+					EnableServiceSync: true,
+					EnableConsole:     false,
+					AuthMode:          "",
+					User:              "",
+					Password:          "",
+					ClusterLocal:      true,
+				},
+			})
+			assert.Check(t, err, "Unable to create VAN router")
+		}
 
 		// Create the VAN Service Interface.
 		service := types.ServiceInterface{
@@ -214,22 +236,29 @@ func TestVanServiceInterfaceCreate(t *testing.T) {
 			Protocol: testcase.proto,
 			Port:     testcase.port,
 		}
-		err = cli.VanServiceInterfaceCreate(ctx, &service)
+		observedError := cli.VanServiceInterfaceCreate(ctx, &service)
 
 		// Check error returns against what was expected.
-		switch testcase.err {
+		switch testcase.expectedErr {
 		case "":
-			assert.Check(t, err == nil, "Test %s failure: %s  An error was reported where none was expected.\n", testcase.name, testcase.doc)
-		case "error":
-			assert.Check(t, err != nil, "Test %s failure: %s No error was reported, but one should have been.\n", testcase.name, testcase.doc)
+			assert.Check(t, observedError == nil || strings.Contains(observedError.Error(), "already defined"), "Test %s failure: An error was reported where none was expected. The error was |%s|.\n", testcase.namespace, observedError)
 		default:
-			assert.Check(t, err == nil || testcase.err == err.Error(), "Test %s failure: %s The reported error was different from the expected error.\n", testcase.name, testcase.doc)
+			if observedError == nil {
+				assert.Check(t, observedError != nil, "Test %s failure: The expected error |%s| was not reported.\n", testcase.namespace, testcase.expectedErr)
+			} else {
+				assert.Check(t, strings.Contains(observedError.Error(), testcase.expectedErr), "Test %s failure: The reported error |%s| did not have the expected prefix |%s|.\n", testcase.namespace, observedError.Error(), testcase.expectedErr)
+			}
 		}
 
 		// Check all the lists of expected entities.
-		check_result(t, testcase.timeout, "dependencies", testcase.depsExpected, &depsFound, testcase.doc)
-		check_result(t, testcase.timeout, "config maps", testcase.cmsExpected, &cmsFound, testcase.doc)
-		check_result(t, testcase.timeout, "roles", testcase.rolesExpected, &rolesFound, testcase.doc)
-		check_result(t, testcase.timeout, "services", testcase.svcsExpected, &svcsFound, testcase.doc)
+		check_result(t, testcase.namespace, testcase.timeout, "dependencies", testcase.depsExpected, &depsFound, testcase.doc)
+		check_result(t, testcase.namespace, testcase.timeout, "config maps", testcase.cmsExpected, &cmsFound, testcase.doc)
+		check_result(t, testcase.namespace, testcase.timeout, "roles", testcase.rolesExpected, &rolesFound, testcase.doc)
+
+		if isCluster {
+			check_result(t, testcase.namespace, testcase.timeout, "services", testcase.realSvcsExpected, &svcsFound, testcase.doc)
+		} else {
+			check_result(t, testcase.namespace, testcase.timeout, "services", testcase.svcsExpected, &svcsFound, testcase.doc)
+		}
 	}
 }
