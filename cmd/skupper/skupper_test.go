5e56e0c223dacf13b89884be8ab8f1f1ba809023
Nicolas Brignone <nmbrignone@gmail.com>
2020-08-26 18:22:44 -0300

    Testing unexposeRun.

--- cmd/skupper/skupper_test.go
+++ cmd/skupper/skupper_test.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"gotest.tools/assert"
 )
+
+func TestMain(m *testing.M) {
+	silenceCobra()
+	os.Exit(m.Run())
+}
 
 func Test_requiredArg(t *testing.T) {
 	r := func(args []string) error {
@@ -89,4 +99,83 @@ func Test_exposeTargetArgs(t *testing.T) {
 	for _, target := range validExposeTargets {
 		assert.Assert(t, e([]string{target, "name"}))
 	}
+}
+
+type serviceInterfaceUnbindCallArgs struct {
+	targetType, targetName, address string
+	deleteIfNoTargets               bool
+}
+
+type vanClientMock struct {
+	serviceInterfaceUnbindCalledWith   []serviceInterfaceUnbindCallArgs
+	serviceInterfaceUnbindReturnsError string
+}
+
+func (v *vanClientMock) ServiceInterfaceUnbind(ctx context.Context, targetType string, targetName string, address string, deleteIfNoTargets bool) error {
+	var calledWith = serviceInterfaceUnbindCallArgs{
+		targetType:        targetType,
+		targetName:        targetName,
+		address:           address,
+		deleteIfNoTargets: deleteIfNoTargets,
+	}
+	v.serviceInterfaceUnbindCalledWith = append(v.serviceInterfaceUnbindCalledWith, calledWith)
+
+	if v.serviceInterfaceUnbindReturnsError != "" {
+		return fmt.Errorf("%s", v.serviceInterfaceUnbindReturnsError)
+	}
+
+	return nil
+}
+
+func Test_cmdUnexpose(t *testing.T) {
+	test := func(targetType, targetName, address string, injectedError string) {
+		options := Options{
+			unexposeAddress: address,
+		}
+		cli := vanClientMock{}
+		cli.serviceInterfaceUnbindReturnsError = injectedError
+
+		args := []string{targetType}
+
+		//supporting "targetType TargetName" and "targetType/targetName" notations
+		if targetName != "" {
+			args = append(args, targetName)
+		} else {
+			parts := strings.Split(targetType, "/")
+			targetType = parts[0]
+			targetName = parts[1]
+		}
+
+		err := unexposeRun(nil, args, options, &cli)
+
+		if injectedError != "" {
+			assert.Error(t, err, "Error, unable to skupper service: "+injectedError)
+		} else {
+			assert.Assert(t, err)
+		}
+
+		assert.Equal(t, len(cli.serviceInterfaceUnbindCalledWith), 1)
+
+		expected := serviceInterfaceUnbindCallArgs{
+			targetType:        targetType,
+			targetName:        targetName,
+			address:           address,
+			deleteIfNoTargets: true}
+
+		assert.Assert(t, cmp.Equal(cli.serviceInterfaceUnbindCalledWith[0], expected, cmp.AllowUnexported(serviceInterfaceUnbindCallArgs{})))
+	}
+
+	testSuccess := func(targetType, targetName, address string) {
+		test(targetType, targetName, address, "")
+	}
+
+	testError := func(targetType, targetName, address string, errorString string) {
+		test(targetType, targetName, address, errorString)
+	}
+
+	testSuccess("depl", "Name", "theService:8080")
+	testSuccess("depl/Name", "", "theService:8080")
+
+	testError("depl", "Name", "theService:8080", "some error")
+	testError("depl/Name", "", "theService:8080", "other error")
 }
