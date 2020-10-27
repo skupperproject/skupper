f06da9cffa73d569e065d7d70bb5be5af65f931b
nicob87 <nmbrignone@gmail.com>
2020-10-27 10:23:57 -0300

    change Echo test function to public, will be used also for skupper-docker tests (#307)

--- test/integration/tcp_echo/tcp_echo.go
+++ test/integration/tcp_echo/tcp_echo.go
@@ -3,6 +3,9 @@ package tcp_echo
 import (
 	"context"
 	"fmt"
+	"log"
+	"net"
+	"strings"
 	"testing"
 	"time"
 
@@ -140,6 +143,66 @@ func (r *TcpEchoClusterTestRunner) Setup(ctx context.Context, t *testing.T) {
 
 	err = pub1Cluster.VanClient.ServiceInterfaceBind(ctx, &service, "deployment", "tcp-go-echo", "tcp", 0)
 	assert.Assert(t, err)
+}
+
+func SendReceive(addr string) error {
+	doneCh := make(chan error)
+	go func(doneCh chan error) {
+
+		strEcho := "Halo"
+		log.Println("Resolving TCP Address")
+		tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+		if err != nil {
+			doneCh <- fmt.Errorf("ResolveTCPAddr failed: %s\n", err.Error())
+			return
+		}
+
+		log.Println("Opening TCP connection")
+		conn, err := net.DialTCP("tcp", nil, tcpAddr)
+		if err != nil {
+			doneCh <- fmt.Errorf("Dial failed: %s\n", err.Error())
+			return
+		}
+		defer conn.Close()
+
+		log.Println("Sending data")
+		_, err = conn.Write([]byte(strEcho))
+		if err != nil {
+			doneCh <- fmt.Errorf("Write to server failed: %s\n", err.Error())
+			return
+		}
+
+		log.Println("Receiving reply")
+		reply := make([]byte, 1024)
+
+		_, err = conn.Read(reply)
+		if err != nil {
+			doneCh <- fmt.Errorf("Read from server failed: %s\n", err.Error())
+			return
+		}
+
+		log.Println("Sent to server = ", strEcho)
+		log.Println("Reply from server = ", string(reply))
+
+		if !strings.Contains(string(reply), strings.ToUpper(strEcho)) {
+			doneCh <- fmt.Errorf("Response from server different that expected: %s\n", string(reply))
+			return
+		}
+
+		doneCh <- nil
+	}(doneCh)
+	timeoutCh := time.After(time.Minute)
+
+	// TCP Echo Client job sometimes hangs waiting for response
+	// This will cause job to fail and a retry to occur
+	var err error
+	select {
+	case err = <-doneCh:
+	case <-timeoutCh:
+		err = fmt.Errorf("timed out waiting for tcp-echo job to finish")
+	}
+
+	return err
 }
 
 func (r *TcpEchoClusterTestRunner) Run(ctx context.Context, t *testing.T) {
--- test/integration/tcp_echo/tcp_echo_test.go
+++ test/integration/tcp_echo/tcp_echo_test.go
@@ -4,13 +4,8 @@ package tcp_echo
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"net"
 	"os"
-	"strings"
 	"testing"
-	"time"
 
 	"github.com/skupperproject/skupper/test/utils/base"
 	"github.com/skupperproject/skupper/test/utils/k8s"
@@ -41,68 +36,7 @@ func TestTcpEcho(t *testing.T) {
 	testRunner.Run(ctx, t)
 }
 
-func sendReceive() error {
-	doneCh := make(chan error)
-	go func(doneCh chan error) {
-		servAddr := "tcp-go-echo:9090"
-
-		strEcho := "Halo"
-		log.Println("Resolving TCP Address")
-		tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-		if err != nil {
-			doneCh <- fmt.Errorf("ResolveTCPAddr failed: %s\n", err.Error())
-			return
-		}
-
-		log.Println("Opening TCP connection")
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
-		if err != nil {
-			doneCh <- fmt.Errorf("Dial failed: %s\n", err.Error())
-			return
-		}
-		defer conn.Close()
-
-		log.Println("Sending data")
-		_, err = conn.Write([]byte(strEcho))
-		if err != nil {
-			doneCh <- fmt.Errorf("Write to server failed: %s\n", err.Error())
-			return
-		}
-
-		log.Println("Receiving reply")
-		reply := make([]byte, 1024)
-
-		_, err = conn.Read(reply)
-		if err != nil {
-			doneCh <- fmt.Errorf("Read from server failed: %s\n", err.Error())
-			return
-		}
-
-		log.Println("Sent to server = ", strEcho)
-		log.Println("Reply from server = ", string(reply))
-
-		if !strings.Contains(string(reply), strings.ToUpper(strEcho)) {
-			doneCh <- fmt.Errorf("Response from server different that expected: %s\n", string(reply))
-			return
-		}
-
-		doneCh <- nil
-	}(doneCh)
-	timeoutCh := time.After(time.Minute)
-
-	// TCP Echo Client job sometimes hangs waiting for response
-	// This will cause job to fail and a retry to occur
-	var err error
-	select {
-	case err = <-doneCh:
-	case <-timeoutCh:
-		err = fmt.Errorf("timed out waiting for tcp-echo job to finish")
-	}
-
-	return err
-}
-
 func TestTcpEchoJob(t *testing.T) {
 	k8s.SkipTestJobIfMustBeSkipped(t)
-	assert.Assert(t, sendReceive())
+	assert.Assert(t, SendReceive("tcp-go-echo:9090"))
 }
