42dfbc4571d765a16b59bcb89713fb174abe9f08
mgoulish <mgoulish@redhat.com>
2020-10-28 01:48:22 -0400

    Remove tests that need multi-connection

--- client/connector_create_test.go
+++ client/connector_create_test.go
@@ -4,19 +4,11 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/skupperproject/skupper/api/types"
 	"github.com/skupperproject/skupper/pkg/kube"
 	"gotest.tools/assert"
-
-	corev1 "k8s.io/api/core/v1"
-	corev1informer "k8s.io/client-go/informers/core/v1"
-	"k8s.io/client-go/tools/cache"
 )
 
 func TestConnectorCreateError(t *testing.T) {
@@ -31,137 +23,6 @@ func TestConnectorCreateError(t *testing.T) {
 		Cost: 1,
 	})
 	assert.ErrorContains(t, err, "open ./somefile.yaml: no such file or directory", "Expect error when file not found")
-}
-
-func TestConnectorCreateInterior(t *testing.T) {
-	if !*clusterRun {
-		lightRed := "\033[1;31m"
-		resetColor := "\033[0m"
-		t.Skip(fmt.Sprintf("%sSkipping: This test only works in real clusters.%s", string(lightRed), string(resetColor)))
-		return
-	}
-
-	testcases := []struct {
-		doc             string
-		expectedError   string
-		connName        string
-		connFile        string
-		secretsExpected []string
-		opts            []cmp.Option
-	}{
-		{
-			doc:             "Expect generated name to be conn1",
-			expectedError:   "",
-			connName:        "",
-			secretsExpected: []string{"conn1"},
-			opts: []cmp.Option{
-				trans,
-				cmpopts.IgnoreSliceElements(func(v string) bool { return !strings.HasPrefix(v, "conn") }),
-			},
-		},
-		{
-			doc:             "Expect secret name to be as provided: conn2",
-			expectedError:   "",
-			connName:        "conn2",
-			secretsExpected: []string{"conn1", "conn2"},
-			opts: []cmp.Option{
-				trans,
-				cmpopts.IgnoreSliceElements(func(v string) bool { return !strings.HasPrefix(v, "conn") }),
-			},
-		},
-		{
-			doc:             "Expect secret name to be as provided: conn3",
-			expectedError:   "",
-			connName:        "conn3",
-			secretsExpected: []string{"conn1", "conn2", "conn3"},
-			opts: []cmp.Option{
-				trans,
-				cmpopts.IgnoreSliceElements(func(v string) bool { return !strings.HasPrefix(v, "conn") }),
-			},
-		},
-		{
-			doc:             "Expect secret already exists: conn2",
-			expectedError:   "A connector secret of that name already exist, please choose a different name",
-			connName:        "conn2",
-			secretsExpected: []string{"conn1", "conn2", "conn3"},
-			opts: []cmp.Option{
-				trans,
-				cmpopts.IgnoreSliceElements(func(v string) bool { return !strings.HasPrefix(v, "conn") }),
-			},
-		},
-		{
-			doc:             "Expect generated name to be conn4",
-			expectedError:   "",
-			connName:        "",
-			secretsExpected: []string{"conn1", "conn2", "conn3", "conn4"},
-			opts: []cmp.Option{
-				trans,
-				cmpopts.IgnoreSliceElements(func(v string) bool { return !strings.HasPrefix(v, "conn") }),
-			},
-		},
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// Create and set up the two namespaces that we will be using.
-	tokenCreatorNamespace := "van-connector-create-interior"
-	tokenUserNamespace := "van-connector-create-edge"
-	tokenCreatorClient, tokenUserClient, err := setupTwoNamespaces(t, ctx, tokenCreatorNamespace, tokenUserNamespace)
-	assert.Assert(t, err, "Unable to create namespaces")
-	defer kube.DeleteNamespace(tokenCreatorNamespace, tokenCreatorClient.KubeClient)
-	defer kube.DeleteNamespace(tokenUserNamespace, tokenUserClient.KubeClient)
-
-	secretsFound := []string{}
-	secretsInformer := corev1informer.NewSecretInformer(
-		tokenUserClient.KubeClient,
-		tokenUserNamespace,
-		time.Second*30,
-		cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
-	secretsInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			secret := obj.(*corev1.Secret)
-			if !strings.HasPrefix(secret.Name, "skupper") {
-				secretsFound = append(secretsFound, secret.Name)
-			}
-		},
-	})
-
-	go secretsInformer.Run(ctx.Done())
-	cache.WaitForCacheSync(ctx.Done(), secretsInformer.HasSynced)
-
-	testPath := "./tmp/"
-	os.Mkdir(testPath, 0755)
-	defer os.RemoveAll(testPath)
-
-	for _, c := range testcases {
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
-
-		err := tokenCreatorClient.ConnectorTokenCreateFile(ctx, c.connName, testPath+c.connName+".yaml")
-		assert.Assert(t, err, "Unable to create token")
-
-		_, err = tokenUserClient.ConnectorCreateFromFile(ctx, testPath+c.connName+".yaml", types.ConnectorCreateOptions{
-			Name:             c.connName,
-			SkupperNamespace: tokenUserNamespace,
-			Cost:             1,
-		})
-		if c.expectedError == "" {
-			assert.Assert(t, err, "Unable to create connector")
-			// TODO: make more deterministic
-			time.Sleep(time.Second * 1)
-			if diff := cmp.Diff(c.secretsExpected, secretsFound, c.opts...); diff != "" {
-				t.Errorf("TestConnectorCreateInterior "+c.doc+" secrets mismatch (-want +got):\n%s", diff)
-			}
-			//assert.Assert(t, cmp.Equal(c.secretsExpected, secretsFound, trans), c.doc)
-		} else {
-			assert.Error(t, err, c.expectedError, c.doc)
-			if diff := cmp.Diff(c.secretsExpected, secretsFound, c.opts...); diff != "" {
-				t.Errorf("TestConnectorCreateInterior "+c.doc+" secrets mismatch (-want +got):\n%s", diff)
-			}
-		}
-
-	}
 }
 
 func TestSelfConnect(t *testing.T) {
--- client/connector_inspect_test.go
+++ client/connector_inspect_test.go
@@ -2,12 +2,9 @@ package client
 
 import (
 	"context"
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/skupperproject/skupper/api/types"
-	"github.com/skupperproject/skupper/pkg/kube"
 	"gotest.tools/assert"
 )
 
@@ -65,69 +62,5 @@ func TestConnectorInspectNotFound(t *testing.T) {
 	for _, c := range testcases {
 		_, err := cli.ConnectorInspect(ctx, c.connName)
 		assert.Error(t, err, c.expectedError, c.doc)
-	}
-}
-
-func TestConnectorInspectDefaults(t *testing.T) {
-
-	if !*clusterRun {
-		var red string = "\033[1;31m"
-		var resetColor string = "\033[0m"
-		t.Skip(fmt.Sprintf("%sSkipping: This test only works in real clusters.%s", string(red), string(resetColor)))
-		return
-	}
-
-	testcases := []struct {
-		doc           string
-		expectedError string
-		connName      string
-	}{
-		{
-			expectedError: "",
-			doc:           "test one",
-			connName:      "conn1",
-		},
-		{
-			expectedError: "",
-			doc:           "test one",
-			connName:      "conn2",
-		},
-		{
-			expectedError: "",
-			doc:           "test two",
-			connName:      "conn3",
-		},
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	testPath := "./tmp/"
-	os.Mkdir(testPath, 0755)
-	defer os.RemoveAll(testPath)
-
-	// Create and set up the two namespaces that we will be using.
-	tokenCreatorNamespace := "van-connector-inspect-interior"
-	tokenUserNamespace := "van-connector-inspect-edge"
-	tokenCreatorClient, tokenUserClient, err := setupTwoNamespaces(t, ctx, tokenCreatorNamespace, tokenUserNamespace)
-        assert.Assert(t, err, "Can't set up namespaces")
-	defer kube.DeleteNamespace(tokenCreatorNamespace, tokenCreatorClient.KubeClient)
-	defer kube.DeleteNamespace(tokenUserNamespace, tokenUserClient.KubeClient)
-
-	for _, c := range testcases {
-		err := tokenCreatorClient.ConnectorTokenCreateFile(ctx, c.connName, testPath+c.connName+".yaml")
-		assert.Check(t, err, "Unable to create connector token "+c.connName)
-	}
-
-	for _, c := range testcases {
-		_, err := tokenUserClient.ConnectorCreateFromFile(ctx, testPath+c.connName+".yaml", types.ConnectorCreateOptions{
-			Name:             c.connName,
-			SkupperNamespace: tokenUserNamespace,
-			Cost:             1,
-		})
-		assert.Check(t, err, "Unable to create connector for "+c.connName)
-	}
-	for _, c := range testcases {
-		_, err := tokenUserClient.ConnectorInspect(ctx, c.connName)
-		assert.Check(t, err, "Unabled to inspect connector for "+c.connName)
 	}
 }
