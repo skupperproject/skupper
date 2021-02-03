cb8c083b4356966d7cfd81637162b5a3a857ab19
Nicolas Brignone <nmbrignone@gmail.com>
2021-02-03 11:51:26 -0300

    removing comment

--- client/site_config_create.go
+++ client/site_config_create.go
@@ -58,7 +58,7 @@ func (cli *VanClient) SiteConfigCreate(ctx context.Context, spec types.SiteConfi
 	if spec.Password != "" {
 		siteConfig.Data["console-password"] = spec.Password
 	}
-	if spec.Ingress != "" { // XXX ?! isn't this an error?
+	if spec.Ingress != "" {
 		siteConfig.Data["ingress"] = spec.Ingress
 	}
 	// TODO: allow Replicas to be set through skupper-site configmap?
