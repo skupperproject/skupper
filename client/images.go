b4733a928ff66b7342bdef5a51c4d77e96a1e593
Gordon Sim <gsim@redhat.com>
2021-02-11 11:05:00 +0000

    try out 1.15.0 rc

--- client/images.go
+++ client/images.go
@@ -1,6 +1,6 @@
 package client
 
 const (
-	DefaultRouterImage            string = "quay.io/skupper/qdrouterd:master"
+	DefaultRouterImage            string = "quay.io/gordons/qdrouterd:1.15.0-rc1"
 	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.4"
 )
