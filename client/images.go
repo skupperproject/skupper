005104f3213a53188d3b4a5d0419717044eec22c
ajssmith <ansmith@redhat.com>
2021-03-16 16:44:56 -0400

    bump service controller to 0.5 (#447)

--- client/images.go
+++ client/images.go
@@ -2,5 +2,5 @@ package client
 
 const (
 	DefaultRouterImage            string = "quay.io/skupper/qdrouterd:master"
-	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.4"
+	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.5"
 )
