619ef1d5d01d67ef2cfe69bbd9869a2d0a81a0a7
Renato Granzoto <rgranzot@redhat.com>
2021-04-15 11:22:22 -0300

    Add HEY tests for HTTP
    Signed-off-by: Renato Granzoto <rgranzot@redhat.com>

--- test/integration/http/http.go
+++ test/integration/http/http.go
@@ -3,6 +3,7 @@ package http
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -20,6 +21,17 @@ import (
 
 type HttpClusterTestRunner struct {
 	base.ClusterTestRunnerBase
+}
+
+// test table
+type test struct {
+	name            string
+	doc             string
+	cluster         *base.ClusterContext
+	numOfWorkers    string
+	durationOfTests string
+	jobName         string
+	targetURL       string
 }
 
 func int32Ptr(i int32) *int32 { return &i }
@@ -118,6 +130,7 @@ var nghttp2Dep *appsv1.Deployment = &appsv1.Deployment{
 	},
 }
 
+// HTTP2 Load Job
 var h2loadJob = &batchv1.Job{
 	ObjectMeta: metav1.ObjectMeta{
 		Name: "h2load",
@@ -148,7 +161,123 @@ var h2loadJob = &batchv1.Job{
 	},
 }
 
-func (r *HttpClusterTestRunner) RunTests(ctx context.Context, t *testing.T) {
+// Base HTTP1 concurrent requests with Hey
+var h1HeyBaseJob = &batchv1.Job{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "h1heybasejob",
+		//Namespace: namespace,
+	},
+	Spec: batchv1.JobSpec{
+		BackoffLimit: int32Ptr(3),
+		Template: apiv1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "h1heybasejob",
+			},
+			Spec: apiv1.PodSpec{
+				Containers: []apiv1.Container{
+					{
+						Name:            "h1heybase",
+						Image:           "quay.io/skupper/hey",
+						Command:         []string{"hey_linux_amd64"},
+						ImagePullPolicy: apiv1.PullAlways,
+					},
+				},
+				RestartPolicy: apiv1.RestartPolicyNever,
+			},
+		},
+	},
+}
+
+func runHeyTesWithParameter(t *testing.T, cluster *base.ClusterContext, numOfWorkers string, durationOfTests string, jobName string, targetURL string) {
+
+	waitJob := func(cc *base.ClusterContext, jobName string) {
+		t.Helper()
+		job, err := k8s.WaitForJob(cc.Namespace, cc.VanClient.KubeClient, jobName, constants.ImagePullingAndResourceCreationTimeout)
+		assert.Assert(t, err)
+		cc.KubectlExec("logs job/" + jobName)
+		k8s.AssertJob(t, job)
+	}
+
+	jobsClient := cluster.VanClient.KubeClient.BatchV1().Jobs(cluster.Namespace)
+
+	// Set the parameters for Hey
+	h1HeyBaseJob.Spec.Template.Spec.Containers[0].Args = []string{"-c", numOfWorkers, "-z", durationOfTests, targetURL}
+
+	// Set new JobName
+	h1HeyBaseJob.ObjectMeta.Name = jobName
+	h1HeyBaseJob.Spec.Template.Name = jobName
+	h1HeyBaseJob.Spec.Template.Spec.Containers[0].Name = jobName
+
+	_, err := jobsClient.Create(h1HeyBaseJob)
+	assert.Assert(t, err)
+	waitJob(cluster, jobName)
+
+	_output, err := cluster.KubectlExec("logs job/" + jobName)
+	assert.Assert(t, err)
+	output := string(_output)
+
+	// Check if tests passed
+	retCode, errRegex := regexp.MatchString("\\[200\\].[[:digit:]]*.responses", output)
+	assert.Assert(t, errRegex)
+	assert.Assert(t, retCode)
+
+	// Check if any tests did not pass
+	retCode, errRegex = regexp.MatchString("\\[[3-5][0-9]+\\].[[:digit:]]*.responses", output)
+	assert.Assert(t, errRegex)
+	assert.Assert(t, !retCode)
+}
+
+// Create the test table for Hey and start tests
+func runHeyTestTable(t *testing.T, jobCluster *base.ClusterContext) {
+
+	testTable := []test{
+		{
+			name:            "h1hey5wrk30sec",
+			doc:             "Send request using 5 concurrent workers during 30 seconds",
+			cluster:         jobCluster,
+			numOfWorkers:    "5",
+			durationOfTests: "30s",
+			jobName:         "h1hey5wrk30sec",
+			targetURL:       "http://httpbin:8080",
+		},
+		{
+			name:            "h1hey50wrk30sec",
+			doc:             "Send request using 50 concurrent workers during 30 seconds",
+			cluster:         jobCluster,
+			numOfWorkers:    "50",
+			durationOfTests: "30s",
+			jobName:         "h1hey50wrk30sec",
+			targetURL:       "http://httpbin:8080",
+		},
+		{
+			name:            "h1hey5wrk60sec",
+			doc:             "Send request using 5 concurrent workers during 60 seconds",
+			cluster:         jobCluster,
+			numOfWorkers:    "5",
+			durationOfTests: "60s",
+			jobName:         "h1hey5wrk60sec",
+			targetURL:       "http://httpbin:8080",
+		},
+		{
+			name:            "h1hey50wrk60sec",
+			doc:             "Send request using 50 concurrent workers during 60 seconds",
+			cluster:         jobCluster,
+			numOfWorkers:    "50",
+			durationOfTests: "60s",
+			jobName:         "h1hey50wrk60sec",
+			targetURL:       "http://httpbin:8080",
+		},
+	}
+
+	// Iterate over test table
+	for _, test := range testTable {
+		t.Run(test.name, func(t *testing.T) {
+			runHeyTesWithParameter(t, test.cluster, test.numOfWorkers, test.durationOfTests, test.jobName, test.targetURL)
+		})
+	}
+}
+
+func (r *HttpClusterTestRunner) RunTests(t *testing.T) {
 	pubCluster1, err := r.GetPublicContext(1)
 	assert.Assert(t, err)
 
@@ -174,14 +303,20 @@ func (r *HttpClusterTestRunner) RunTests(ctx context.Context, t *testing.T) {
 		k8s.AssertJob(t, job)
 	}
 
+	// Send GET requests via HTTPD1
 	t.Run("http1", func(t *testing.T) {
 		runJob(pubCluster1, "http1", "TestHttpJob")
 		waitJob(pubCluster1, "http1")
 	})
 
+	// Send GET requests via HTTPD2
 	t.Run("http2", func(t *testing.T) {
 		runJob(pubCluster1, "http2", "TestHttp2Job")
 		waitJob(pubCluster1, "http2")
+	})
+
+	// Send a huge load for HTTPD2
+	t.Run("http2load", func(t *testing.T) {
 		jobsClient := pubCluster1.VanClient.KubeClient.BatchV1().Jobs(pubCluster1.Namespace)
 		_, err = jobsClient.Create(h2loadJob)
 		assert.Assert(t, err)
@@ -192,6 +327,9 @@ func (r *HttpClusterTestRunner) RunTests(ctx context.Context, t *testing.T) {
 		output := string(_output)
 		assert.Assert(t, strings.Contains(output, "1000 succeeded"), output)
 	})
+
+	// Call the test table for Hey tests
+	runHeyTestTable(t, pubCluster1)
 }
 
 func (r *HttpClusterTestRunner) Setup(ctx context.Context, t *testing.T) {
@@ -256,5 +394,5 @@ func (r *HttpClusterTestRunner) Setup(ctx context.Context, t *testing.T) {
 func (r *HttpClusterTestRunner) Run(ctx context.Context, t *testing.T) {
 	defer base.TearDownSimplePublicAndPrivate(&r.ClusterTestRunnerBase)
 	r.Setup(ctx, t)
-	r.RunTests(ctx, t)
+	r.RunTests(t)
 }
