656c978be16f820d380c5995891fd64a8b77d774
Gordon Sim <gsim@redhat.com>
2021-05-20 11:21:44 +0100

    test latest router image

--- client/images.go
+++ client/images.go
@@ -1,6 +1,6 @@
 package client
 
 const (
-	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:nightly"
+	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:1.16.0"
 	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.5"
 )
