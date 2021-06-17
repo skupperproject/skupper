a037ad3dd3b0d6c1ac698bb1e0766eb2d56d0774
Gordon Sim <gsim@redhat.com>
2021-06-17 10:18:46 +0100

    Do not merge: test of proposed changes for 1.16.1

--- client/images.go
+++ client/images.go
@@ -1,6 +1,6 @@
 package client
 
 const (
-	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:nightly"
+	DefaultRouterImage            string = "quay.io/gordons/qdrouterd:1.16.1-alpha"
 	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.5"
 )
