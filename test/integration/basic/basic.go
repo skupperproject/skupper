7109497216ce07898c3d61fe713a6f46f0fbac42
Fernando Giorgetti <fgiorget@redhat.com>
2021-07-16 01:30:36 -0300

    Updated basic test to include nodeport validation

--- test/integration/basic/basic.go
+++ test/integration/basic/basic.go
@@ -2,12 +2,20 @@ package basic
 
 import (
 	"context"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/prometheus/common/log"
 	"github.com/skupperproject/skupper/api/types"
 	"github.com/skupperproject/skupper/test/utils/base"
+	"github.com/skupperproject/skupper/test/utils/env"
 	"gotest.tools/assert"
+)
+
+const (
+	SkipReasonIngressNone           = "this test only runs against a single cluster (ingress=none)"
+	SkipReasonNodePortNoIngressHost = "this test can only be executed if PUBLIC_1_INGRESS_HOST environment varialbe is set (ingress=nodeport)"
 )
 
 type BasicTestRunner struct {
@@ -26,7 +34,7 @@ func (r *BasicTestRunner) RunTests(ctx context.Context, t *testing.T) {
 	assert.Assert(t, base.WaitForSkupperConnectedSites(ctx, prvCluster, 1))
 }
 
-func (r *BasicTestRunner) Setup(ctx context.Context, createOptsPublic types.SiteConfigSpec, createOptsPrivate types.SiteConfigSpec, t *testing.T) {
+func (r *BasicTestRunner) Setup(ctx context.Context, createOptsPublic types.SiteConfigSpec, createOptsPrivate types.SiteConfigSpec, tokenType string, t *testing.T) {
 	var err error
 	pub1Cluster, err := r.GetPublicContext(1)
 	assert.Assert(t, err)
@@ -47,7 +55,11 @@ func (r *BasicTestRunner) Setup(ctx context.Context, createOptsPublic types.Site
 	assert.Assert(t, err)
 
 	const secretFile = "/tmp/public_basic_1_secret.yaml"
-	err = pub1Cluster.VanClient.ConnectorTokenCreateFile(ctx, types.DefaultVanName, secretFile)
+	if tokenType == "claim" {
+		err = pub1Cluster.VanClient.TokenClaimCreate(ctx, types.DefaultVanName, []byte(createOptsPublic.Password), 15*time.Minute, 1, secretFile)
+	} else {
+		err = pub1Cluster.VanClient.ConnectorTokenCreateFile(ctx, types.DefaultVanName, secretFile)
+	}
 	assert.Assert(t, err)
 
 	createOptsPrivate.SkupperNamespace = prv1Cluster.Namespace
@@ -87,14 +99,19 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 	assert.Assert(t, err)
 
 	testcases := []struct {
-		doc                string
-		skipOnMultiCluster bool
-		createOptsPublic   types.SiteConfigSpec
-		createOptsPrivate  types.SiteConfigSpec
+		id                string
+		doc               string
+		skip              bool
+		skipReason        string
+		tokenType         string
+		createOptsPublic  types.SiteConfigSpec
+		createOptsPrivate types.SiteConfigSpec
 	}{
 		{
-			doc:                "Connecting, two internals, clusterLocal=true",
-			skipOnMultiCluster: true,
+			id:         "interiors-ingress-none",
+			doc:        "Connecting two interiors with ingress=none",
+			skip:       base.MultipleClusters(),
+			skipReason: SkipReasonIngressNone,
 			createOptsPublic: types.SiteConfigSpec{
 				SkupperName:       "",
 				RouterMode:        string(types.TransportModeInterior),
@@ -121,8 +138,8 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 			},
 		},
 		{
-			doc:                "Connecting, two internals, clusterLocal=false",
-			skipOnMultiCluster: false,
+			id:  "interiors-ingress-default",
+			doc: "Connecting two interiors with ingress=default (route if available or loadbalancer)",
 			createOptsPublic: types.SiteConfigSpec{
 				SkupperName:       "",
 				RouterMode:        string(types.TransportModeInterior),
@@ -149,8 +166,10 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 			},
 		},
 		{
-			doc:                "connecting, Private Edge, Public Internal, clusterLocal=true",
-			skipOnMultiCluster: true,
+			id:         "edge-interior-ingress-none",
+			doc:        "Connecting a private edge to a public interior with ingress=none",
+			skip:       base.MultipleClusters(),
+			skipReason: SkipReasonIngressNone,
 			createOptsPublic: types.SiteConfigSpec{
 				SkupperName:       "",
 				RouterMode:        string(types.TransportModeInterior),
@@ -176,18 +195,56 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 				Replicas:          1,
 			},
 		},
+		{
+			id:         "interiors-ingress-nodeport",
+			doc:        "Connecting two interiors with ingress=nodeport",
+			skip:       os.Getenv(env.Public1IngressHost) == "",
+			skipReason: SkipReasonNodePortNoIngressHost,
+			tokenType:  "claim",
+			createOptsPublic: types.SiteConfigSpec{
+				SkupperName:       "",
+				RouterMode:        string(types.TransportModeInterior),
+				EnableController:  true,
+				EnableServiceSync: true,
+				EnableConsole:     true,
+				AuthMode:          types.ConsoleAuthModeUnsecured,
+				User:              "nicob?",
+				Password:          "nopasswordd",
+				Ingress:           types.IngressNodePortString,
+				Router: types.RouterOptions{
+					IngressHost: os.Getenv(env.Public1IngressHost),
+				},
+				Controller: types.ControllerOptions{
+					IngressHost: os.Getenv(env.Public1IngressHost),
+				},
+				Replicas: 1,
+			},
+			createOptsPrivate: types.SiteConfigSpec{
+				SkupperName:       "",
+				RouterMode:        string(types.TransportModeInterior),
+				EnableController:  true,
+				EnableServiceSync: true,
+				EnableConsole:     false,
+				AuthMode:          types.ConsoleAuthModeUnsecured,
+				User:              "nicob?",
+				Password:          "nopasswordd",
+				Ingress:           pubCluster.VanClient.GetIngressDefault(),
+				Replicas:          1,
+			},
+		},
 	}
 
 	defer r.TearDown(ctx)
 
 	for _, c := range testcases {
-		if c.skipOnMultiCluster && base.MultipleClusters() {
-			t.Logf("Skipping: %s [this test only runs against a single cluster]\n", c.doc)
-			continue
-		}
-		t.Logf("Testing: %s\n", c.doc)
-		r.Setup(ctx, c.createOptsPublic, c.createOptsPrivate, t)
-		r.RunTests(ctx, t)
-		r.TearDown(ctx)
+		t.Run(c.id, func(t *testing.T) {
+			if c.skip {
+				t.Skipf("Skipping: %s [%s]\n", c.doc, c.skipReason)
+			}
+			t.Logf("Testing: %s\n", c.doc)
+			r.Setup(ctx, c.createOptsPublic, c.createOptsPrivate, c.tokenType, t)
+			r.RunTests(ctx, t)
+			r.TearDown(ctx)
+		})
 	}
 }
