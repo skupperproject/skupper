0e8beeeba01bc5af79de214f14c93d6a014db89e
Fernando Giorgetti <fgiorget@redhat.com>
2021-07-16 01:30:36 -0300

    Updated basic test to include nodeport validation

---
+++ test/utils/env/envvars.go
@@ -0,0 +1,10 @@
+//
+// Package env defines environment variables to be used across integration
+// test suites. These variables do not need to be set, but when defined
+// they can modify the behavior of the test suites.
+//
+package env
+
+const (
+	Public1IngressHost = "PUBLIC_1_INGRESS_HOST"
+)
