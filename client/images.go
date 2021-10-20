5ee6e043ed4a5d67b6afe75e05e65ab460719ba4
Gordon Sim <gsim@redhat.com>
2021-10-20 09:57:32 +0100

    update for 0.8

--- client/images.go
+++ client/images.go
@@ -1,6 +1,6 @@
 package client
 
 const (
-	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:nightly"
-	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.7"
+	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:1.17.0"
+	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.8.0"
 )
