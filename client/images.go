3f5a8a0b70e7ccea0957a92049d8e07ff1e63c67
Gordon Sim <gsim@redhat.com>
2021-10-29 09:21:31 +0100

    Test 1.17.1 release candidate

--- client/images.go
+++ client/images.go
@@ -1,6 +1,6 @@
 package client
 
 const (
-	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:nightly"
+	DefaultRouterImage            string = "quay.io/gordons/qdrouterd:1.17.1"
 	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.7"
 )
