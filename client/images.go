1a3e2888c6a5ff20829ced5603b63c77a20aa68b
ajssmith <ansmith@redhat.com>
2021-11-04 16:17:17 -0400

    update the image master uses by default (#630)

--- client/images.go
+++ client/images.go
@@ -2,5 +2,5 @@ package client
 
 const (
 	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:nightly"
-	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.7"
+	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.8.1"
 )
