e9e0cf85f5042aed6a1b91b0ba9dde52e6b14a4a
Gordon Sim <gsim@redhat.com>
2021-11-19 10:17:28 +0000

    update image version tags for 0.8.3

--- client/images.go
+++ client/images.go
@@ -2,5 +2,5 @@ package client
 
 const (
 	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:1.17.1"
-	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.8.2"
+	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:0.8.3"
 )
