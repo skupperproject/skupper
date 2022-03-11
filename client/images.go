210138cdbc416a5011e81a86e34a27d3b2401360
ajssmith <ansmith@redhat.com>
2022-03-11 10:08:28 -0500

    revise default router image

--- client/images.go
+++ client/images.go
@@ -1,7 +1,7 @@
 package client
 
 const (
-	DefaultRouterImage            string = "quay.io/interconnectedcloud/qdrouterd:nightly"
+	DefaultRouterImage            string = "quay.io/skupper/skupper-router:main"
 	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:master"
 	DefaultConfigSyncImage        string = "quay.io/skupper/config-sync:master"
 )
