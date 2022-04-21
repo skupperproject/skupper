f3f59344f8920642c893f89fd19a02222214b395
Gordon Sim <gsim@redhat.com>
2022-04-21 09:49:39 +0100

    prepare for 1.0.0-preview2 release

--- client/images.go
+++ client/images.go
@@ -2,6 +2,6 @@ package client
 
 const (
 	DefaultRouterImage            string = "quay.io/skupper/skupper-router:2.0.0"
-	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:1.0.0-preview"
-	DefaultConfigSyncImage        string = "quay.io/skupper/config-sync:1.0.0-preview"
+	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:1.0.0-preview2"
+	DefaultConfigSyncImage        string = "quay.io/skupper/config-sync:1.0.0-preview2"
 )
