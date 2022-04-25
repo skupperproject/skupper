46806988639c7b566114dcac9dab930bade89be5
Renato Granzoto <rgranzot@redhat.com>
2022-04-25 15:29:44 -0300

    Adding test for config-sync
    Signed-off-by: Renato Granzoto <rgranzot@redhat.com>

--- test/integration/acceptance/custom/basic/basic.go
+++ test/integration/acceptance/custom/basic/basic.go
@@ -2,10 +2,6 @@ package basic
 
 import (
 	"context"
-	"os"
-	"testing"
-	"time"
-
 	"github.com/prometheus/common/log"
 	"github.com/skupperproject/skupper/api/types"
 	"github.com/skupperproject/skupper/pkg/kube"
@@ -14,6 +10,10 @@ import (
 	"github.com/skupperproject/skupper/test/utils/constants"
 	"github.com/skupperproject/skupper/test/utils/env"
 	"gotest.tools/assert"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"os"
+	"testing"
+	"time"
 )
 
 const (
@@ -37,7 +37,7 @@ func (r *BasicTestRunner) RunTests(ctx context.Context, t *testing.T) {
 	assert.Assert(t, base.WaitForSkupperConnectedSites(ctx, prvCluster, 1))
 }
 
-func (r *BasicTestRunner) Setup(ctx context.Context, createOptsPublic types.SiteConfigSpec, createOptsPrivate types.SiteConfigSpec, tokenType string, t *testing.T) {
+func (r *BasicTestRunner) Setup(ctx context.Context, createOptsPublic types.SiteConfigSpec, createOptsPrivate types.SiteConfigSpec, tokenType string, testSync bool, t *testing.T) {
 	var err error
 	pub1Cluster, err := r.GetPublicContext(1)
 	assert.Assert(t, err)
@@ -65,13 +65,53 @@ func (r *BasicTestRunner) Setup(ctx context.Context, createOptsPublic types.Site
 	err = prv1Cluster.VanClient.RouterCreate(ctx, *siteConfig)
 	assert.Assert(t, err)
 
-	var connectorCreateOpts types.ConnectorCreateOptions = types.ConnectorCreateOptions{
+	var podStartTimeBefore *v1.Time
+	if testSync == true {
+		// Pick the pod details for config-sync validation
+		podsRouter, _ := kube.GetPods("skupper.io/component=router", prv1Cluster.Namespace, prv1Cluster.VanClient.KubeClient)
+		assert.Assert(t, len(podsRouter) > 0)
+		podStartTimeBefore = podsRouter[0].Status.StartTime
+	}
+
+	var connectorCreateOpts = types.ConnectorCreateOptions{
 		SkupperNamespace: prv1Cluster.Namespace,
 		Name:             "",
 		Cost:             0,
 	}
 	_, err = prv1Cluster.VanClient.ConnectorCreateFromFile(ctx, secretFile, connectorCreateOpts)
 	assert.Assert(t, err)
+
+	var podStartTimeAfter *v1.Time
+	if testSync == true {
+		assert.Assert(t, base.WaitForSkupperConnectedSites(ctx, prv1Cluster, 1))
+
+		linkStatus, err := prv1Cluster.VanClient.ConnectorInspect(context.Background(), "link1")
+		assert.Assert(t, err)
+		if err != nil {
+			log.Fatalf("[TestSync] error: %v", err)
+			return
+		}
+		assert.Assert(t, linkStatus.Connected == true)
+
+		podsRouter, _ := kube.GetPods("skupper.io/component=router", prv1Cluster.Namespace, prv1Cluster.VanClient.KubeClient)
+		assert.Assert(t, len(podsRouter) > 0)
+		podStartTimeAfter = podsRouter[0].Status.StartTime
+
+		// Check if the container has restarted
+		assert.Assert(t, podStartTimeAfter.Equal(podStartTimeBefore))
+
+		// Check if the Volume is shared by both containers
+		podContainers, _ := kube.GetReadyPod(prv1Cluster.Namespace, prv1Cluster.VanClient.KubeClient, "router")
+		for _, container := range podContainers.Spec.Containers {
+			foundCertVol := false
+			for _, contVolume := range container.VolumeMounts {
+				if contVolume.Name == "skupper-router-certs" {
+					foundCertVol = true
+				}
+			}
+			assert.Assert(t, foundCertVol == true)
+		}
+	}
 }
 
 func (r *BasicTestRunner) Delete(ctx context.Context, t *testing.T) {
@@ -117,14 +157,16 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 		skip              bool
 		skipReason        string
 		tokenType         string
+		testSync          bool
 		createOptsPublic  types.SiteConfigSpec
 		createOptsPrivate types.SiteConfigSpec
 	}{
 		{
-			id:         "interiors-ingress-none",
-			doc:        "Connecting two interiors with ingress=none",
+			id:         "test-sync-container",
+			doc:        "Test the config-sync container",
 			skip:       base.MultipleClusters(),
 			skipReason: SkipReasonIngressNone,
+			testSync:   true,
 			createOptsPublic: types.SiteConfigSpec{
 				SkupperName:       "",
 				RouterMode:        string(types.TransportModeInterior),
@@ -151,8 +193,40 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 			},
 		},
 		{
-			id:  "interiors-ingress-default",
-			doc: "Connecting two interiors with ingress=default (route if available or loadbalancer)",
+			id:         "interiors-ingress-none",
+			doc:        "Connecting two interiors with ingress=none",
+			skip:       base.MultipleClusters(),
+			skipReason: SkipReasonIngressNone,
+			testSync:   false,
+			createOptsPublic: types.SiteConfigSpec{
+				SkupperName:       "",
+				RouterMode:        string(types.TransportModeInterior),
+				EnableController:  true,
+				EnableServiceSync: true,
+				EnableConsole:     false,
+				AuthMode:          types.ConsoleAuthModeUnsecured,
+				User:              "nicob?",
+				Password:          "nopasswordd",
+				Ingress:           types.IngressNoneString,
+				Replicas:          1,
+			},
+			createOptsPrivate: types.SiteConfigSpec{
+				SkupperName:       "",
+				RouterMode:        string(types.TransportModeInterior),
+				EnableController:  true,
+				EnableServiceSync: true,
+				EnableConsole:     false,
+				AuthMode:          types.ConsoleAuthModeUnsecured,
+				User:              "nicob?",
+				Password:          "nopasswordd",
+				Ingress:           types.IngressNoneString,
+				Replicas:          1,
+			},
+		},
+		{
+			id:       "interiors-ingress-default",
+			doc:      "Connecting two interiors with ingress=default (route if available or loadbalancer)",
+			testSync: false,
 			createOptsPublic: types.SiteConfigSpec{
 				SkupperName:       "",
 				RouterMode:        string(types.TransportModeInterior),
@@ -183,6 +257,7 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 			doc:        "Connecting a private edge to a public interior with ingress=none",
 			skip:       base.MultipleClusters(),
 			skipReason: SkipReasonIngressNone,
+			testSync:   false,
 			createOptsPublic: types.SiteConfigSpec{
 				SkupperName:       "",
 				RouterMode:        string(types.TransportModeInterior),
@@ -214,6 +289,7 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 			skip:       os.Getenv(env.Public1IngressHost) == "",
 			skipReason: SkipReasonNodePortNoIngressHost,
 			tokenType:  "claim",
+			testSync:   false,
 			createOptsPublic: types.SiteConfigSpec{
 				SkupperName:       "",
 				RouterMode:        string(types.TransportModeInterior),
@@ -264,7 +340,7 @@ func (r *BasicTestRunner) Run(ctx context.Context, t *testing.T) {
 			}
 			t.Logf("Testing: %s\n", c.doc)
 			defer r.Delete(ctx, t)
-			r.Setup(ctx, c.createOptsPublic, c.createOptsPrivate, c.tokenType, t)
+			r.Setup(ctx, c.createOptsPublic, c.createOptsPrivate, c.tokenType, c.testSync, t)
 			r.RunTests(ctx, t)
 		})
 	}
