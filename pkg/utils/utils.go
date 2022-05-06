17f1fb09a242ae17e35d2ae8b7a9a9469ca50e5d
Noe Luaces <nluaces@redhat.com>
2022-05-06 11:24:49 +0200

    Fixed error when a HTTP2 connector/listener was synchronised (#736)
    * Fixed error when a HTTP2 connector/listener was created without creating the ssl profile first in the router.
    
    * Added types for functions passed as a parameter. Moving  sslProfiles from HttpEndpoints to the BrigdeConfigDifference
    
    * Checked if sslProfiles have a single use before deleting them
    
    * Added verifyHostName to httpEndpoint model for TLS httpConnectors.

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -115,3 +115,15 @@ func IsDirEmpty(name string) (bool, error) {
 
 	return false, err
 }
+
+func StringSlicesEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
