a861728adfec261d2db2c8b5085fc99bcca3d4be
Noe Luaces <nluaces@redhat.com>
2022-06-14 11:53:02 +0200

    Update for 1.0.2

--- client/images.go
+++ client/images.go
@@ -2,6 +2,6 @@ package client
 
 const (
 	DefaultRouterImage            string = "quay.io/skupper/skupper-router:2.0.2"
-	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:1.0.1"
-	DefaultConfigSyncImage        string = "quay.io/skupper/config-sync:1.0.1"
+	DefaultServiceControllerImage string = "quay.io/skupper/service-controller:1.0.2"
+	DefaultConfigSyncImage        string = "quay.io/skupper/config-sync:1.0.2"
 )
