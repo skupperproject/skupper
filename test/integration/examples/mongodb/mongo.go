e24b190d1142bb117984003d66dcdd93c79eec2c
Danilo Gonzalez Hashimoto <dhashimo@redhat.com>
2022-07-28 19:06:39 -0300

    Fix for issues caused by requests before election settles
    - properly fail test if replicaset creation fails straight off
    - wait for mongo replicaset primary member election to complete
    - add a third member to the replicaset.  Best practice and speeds up
      elections
    - show rs.status() at the end of the test, for helping debug
    
    Signed-off-by: Danilo Gonzalez Hashimoto <dhashimo@redhat.com>

--- test/integration/examples/mongodb/mongo.go
+++ test/integration/examples/mongodb/mongo.go
@@ -2,10 +2,15 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/skupperproject/skupper/api/types"
 	vanClient "github.com/skupperproject/skupper/client"
+	"github.com/skupperproject/skupper/pkg/utils"
 	"github.com/skupperproject/skupper/test/utils/base"
 	"github.com/skupperproject/skupper/test/utils/constants"
 	"github.com/skupperproject/skupper/test/utils/k8s"
@@ -21,12 +26,53 @@ func RunTests(ctx context.Context, t *testing.T, r *base.ClusterTestRunnerBase) 
 	prvCluster1, err := r.GetPrivateContext(1)
 	assert.Assert(t, err)
 
+	defer func() {
+		log.Print("Replicaset status at the end of the test")
+		prvCluster1.KubectlExec(`exec deploy/mongo-a -- mongo --host 127.0.0.1:27017 --eval 'rs.status()'`)
+	}()
+
 	_, err = k8s.WaitForSkupperServiceToBeCreatedAndReadyToUse(pubCluster1.Namespace, pubCluster1.VanClient.KubeClient, "mongo-a")
 	assert.Assert(t, err)
 	_, err = k8s.WaitForSkupperServiceToBeCreatedAndReadyToUse(prvCluster1.Namespace, prvCluster1.VanClient.KubeClient, "mongo-b")
 	assert.Assert(t, err)
+	_, err = k8s.WaitForSkupperServiceToBeCreatedAndReadyToUse(pubCluster1.Namespace, pubCluster1.VanClient.KubeClient, "mongo-c")
+	assert.Assert(t, err)
 
-	_, err = prvCluster1.KubectlExec(`exec deploy/mongo-a -- mongo --host 127.0.0.1:27017 --eval 'rs.initiate({ _id : "rs0", members: [ { _id: 0, host: "mongo-a:27017" }, { _id: 1, host: "mongo-b:27017" }]})'`)
+	// Make sure you have a minimum of three members on the replicaset
+	// https://www.mongodb.com/docs/manual/tutorial/deploy-replica-set/
+	// https://www.mongodb.com/docs/manual/core/replica-set-architecture-three-members/
+	_, err = prvCluster1.KubectlExec(
+		`exec deploy/mongo-a -- mongo --host 127.0.0.1:27017 --eval '
+		var resp = rs.initiate(
+			{ 
+				_id : "rs0", 
+				members: [ 
+					{ _id: 0, host: "mongo-a:27017", priority: 3 }, 
+					{ _id: 1, host: "mongo-b:27017", priority: 2 },
+					{ _id: 2, host: "mongo-c:27017", priority: 1 }
+				]
+			}
+		); 
+		printjson (resp);
+		if (resp.ok != 1) {
+			quit(1);
+		}'
+	`)
+	assert.Assert(t, err)
+
+	// Let's wait until the election is settled, for a maximum of 5 min
+	log.Print("Waiting for the Mongo replicaset primary member election to be settled")
+	err = utils.RetryError(5*time.Second, 12*5, func() error {
+		out, err := prvCluster1.KubectlExec(`exec deploy/mongo-a -- mongo --host 127.0.0.1:27017 --eval 'db.hello().primary' --quiet`)
+		if err != nil {
+			return err
+		}
+		if string(out) == "" {
+			return fmt.Errorf("No primary elected for the Mongo replicaset")
+		}
+		log.Printf("Mongo primary election settled: %v.  Proceeding", strings.TrimSpace(string(out)))
+		return nil
+	})
 	assert.Assert(t, err)
 
 	jobName := "mongo"
@@ -59,6 +105,9 @@ func Setup(ctx context.Context, t *testing.T, r *base.ClusterTestRunnerBase) {
 	_, err = pub1Cluster.KubectlExec("apply -f https://raw.githubusercontent.com/skupperproject/skupper-example-mongodb-replica-set/master/deployment-mongo-b.yaml")
 	assert.Assert(t, err)
 
+	_, err = pub1Cluster.KubectlExec("apply -f https://raw.githubusercontent.com/skupperproject/skupper-example-mongodb-replica-set/master/deployment-mongo-c.yaml")
+	assert.Assert(t, err)
+
 	expose := func(name string, cli *vanClient.VanClient) {
 		t.Helper()
 		service := types.ServiceInterface{
@@ -76,7 +125,8 @@ func Setup(ctx context.Context, t *testing.T, r *base.ClusterTestRunnerBase) {
 	}
 	expose("mongo-a", prv1Cluster.VanClient)
 	expose("mongo-b", pub1Cluster.VanClient)
-	// Just need to do load orr rs inittiate, the easiest one.
+	expose("mongo-c", pub1Cluster.VanClient)
+	// Just need to do load orr rs initiate, the easiest one.
 
 }
 
