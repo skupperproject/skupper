4dca78865c564e291c16990d5be2e6de690ed8e9
Gordon Sim <gsim@redhat.com>
2022-09-30 17:39:18 +0100

    TEST_ONLY: change router image

--- client/images.go
+++ client/images.go
@@ -2,7 +2,7 @@ package client
 
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
-	RouterImageName            string = "skupper-router:main"
+	RouterImageName            string = "skupper-router:2.2.1"
 	ServiceControllerImageName string = "service-controller:master"
 	ConfigSyncImageName        string = "config-sync:master"
 	FlowCollectorImageName     string = "vflow-collector:master"
