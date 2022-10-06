1afc3bdafc80b3997981c792e7c3f3b66ba8ff5e
Gordon Sim <gsim@redhat.com>
2022-10-06 19:25:09 +0100

    update image tags

--- client/images.go
+++ client/images.go
@@ -3,6 +3,6 @@ package client
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
 	RouterImageName            string = "skupper-router:2.1.0"
-	ServiceControllerImageName string = "service-controller:1.1.0"
-	ConfigSyncImageName        string = "config-sync:1.1.0"
+	ServiceControllerImageName string = "service-controller:1.1.1"
+	ConfigSyncImageName        string = "config-sync:1.1.1"
 )
