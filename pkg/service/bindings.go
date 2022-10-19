cc8f767693978b4cb158c5a7539c3082d8f02070
Noe Luaces <nluaces@redhat.com>
2022-10-19 18:49:05 +0200

    Enable to expose and create services over TLS for the TCP protocol

--- pkg/service/bindings.go
+++ pkg/service/bindings.go
@@ -410,13 +410,23 @@ func addEgressBridge(protocol string, host string, port map[int]int, address str
 			}
 			bridges.AddHttpConnector(httpConnector)
 		case ProtocolTCP:
-			bridges.AddTcpConnector(qdr.TcpEndpoint{
+			tcpConnector := qdr.TcpEndpoint{
 				Name:    endpointName,
 				Host:    host,
 				Port:    strconv.Itoa(tPort),
 				Address: endpointAddr,
 				SiteId:  siteId,
-			})
+			}
+
+			if len(tlsCredentials) > 0 {
+				verifyHostName := new(bool)
+				*verifyHostName = false
+				tcpConnector.SslProfile = types.ServiceClientSecret
+				tcpConnector.VerifyHostname = verifyHostName
+			}
+
+			bridges.AddTcpConnector(tcpConnector)
+
 		default:
 			return false, fmt.Errorf("Unrecognised protocol for service %s: %s", address, protocol)
 		}
@@ -462,12 +472,19 @@ func addIngressBridge(sb *ServiceBindings, siteId string, bridges *qdr.BridgeCon
 
 			bridges.AddHttpListener(httpListener)
 		case ProtocolTCP:
-			bridges.AddTcpListener(qdr.TcpEndpoint{
+			tcpListener := qdr.TcpEndpoint{
 				Name:    endpointName,
 				Port:    strconv.Itoa(iPort),
 				Address: endpointAddr,
 				SiteId:  siteId,
-			})
+			}
+
+			if len(sb.tlsCredentials) > 0 {
+				tcpListener.SslProfile = sb.tlsCredentials
+			}
+
+			bridges.AddTcpListener(tcpListener)
+
 		default:
 			return false, fmt.Errorf("Unrecognised protocol for service %s: %s", sb.Address, sb.protocol)
 		}
