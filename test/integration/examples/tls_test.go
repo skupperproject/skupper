81bdb6e313d6206d906c9ee10e896fac5b23e8db
Danilo Gonzalez Hashimoto <dhashimo@redhat.com>
2022-11-16 11:05:04 -0300

    initial for tls testing

---
+++ test/integration/examples/tls_test.go
@@ -0,0 +1,24 @@
+//go:build integration || smoke || examples
+// +build integration smoke examples
+
+package examples
+
+import (
+	"context"
+	"testing"
+
+	//"github.com/skupperproject/skupper/test/integration/examples/tls_test"
+	//"skupper/test/integration/examples/tls_test"
+
+	//"tls_t"
+	"github.com/skupperproject/skupper/test/integration/examples/tls_t"
+
+	"github.com/skupperproject/skupper/test/utils/constants"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+)
+
+func TestTls(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), constants.ImagePullingAndResourceCreationTimeout)
+	defer cancelFn()
+	tls_t.Run(ctx, t, testRunner)
+}
