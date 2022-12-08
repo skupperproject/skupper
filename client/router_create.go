8d1f572dac73404634b179527cd75e8e9808cb71
iago.david <iagodvsantos@gmail.com>
2022-12-08 14:41:15 -0300

    fix test

--- client/router_create.go
+++ client/router_create.go
@@ -463,7 +463,13 @@ func (cli *VanClient) ClusterRoles(enablePermissions bool) []*rbacv1.ClusterRole
 			{
 				APIGroups: []string{""},
 				Resources: []string{"namespaces"},
-				Verbs:     []string{"get"}},
+				Verbs:     []string{"get"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{"nodes"},
+				Verbs:     []string{"get", "list", "watch"},
+			},
 		}, controllerRules...),
 	})
 	return clusterRoles
