0a6d42879a1d67240b580fc4b54bd6b59ca21a08
Fernando Giorgetti <fgiorget@redhat.com>
2023-02-09 15:44:18 -0300

    Adjusted expose unit test

--- cmd/skupper/skupper_test.go
+++ cmd/skupper/skupper_test.go
@@ -90,7 +90,7 @@ func TestCreateServiceParseArgs(t *testing.T) {
 
 func TestExposeTargetArgs(t *testing.T) {
 	s := &SkupperKubeService{}
-	genericError := "expose target and name must be specified (e.g. 'skupper expose deployment <name>'"
+	genericError := "expose target and name must be specified (e.g. 'skupper expose deployment <name>')"
 	targetError := "target type must be one of: [deployment, statefulset, pods, service, deploymentconfig]"
 
 	e := func(args []string) error {
