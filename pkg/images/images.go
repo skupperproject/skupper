26da1caa6110505438f46c1749ffd871f54f9518
Noe Luaces <nluaces@redhat.com>
2023-03-10 12:44:22 +0100

    update for 1.3

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -2,8 +2,8 @@ package images
 
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
-	RouterImageName            string = "skupper-router:2.3.0"
-	ServiceControllerImageName string = "service-controller:1.3.0-preview"
-	ConfigSyncImageName        string = "config-sync:1.3.0-preview"
-	FlowCollectorImageName     string = "flow-collector:1.3.0-preview"
+	RouterImageName            string = "skupper-router:2.3.2"
+	ServiceControllerImageName string = "service-controller:1.3.0"
+	ConfigSyncImageName        string = "config-sync:1.3.0"
+	FlowCollectorImageName     string = "flow-collector:1.3.0"
 )
