b58751acf41ad7f8f648cc5ba51ea762a82d6a50
Gordon Sim <gsim@redhat.com>
2023-05-04 12:28:25 +0100

    bump image versions in preparation for respin

--- client/images.go
+++ client/images.go
@@ -3,6 +3,6 @@ package client
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
 	RouterImageName            string = "skupper-router:2.2.1"
-	ServiceControllerImageName string = "service-controller:1.2.5"
-	ConfigSyncImageName        string = "config-sync:1.2.5"
+	ServiceControllerImageName string = "service-controller:1.2.6"
+	ConfigSyncImageName        string = "config-sync:1.2.6"
 )
