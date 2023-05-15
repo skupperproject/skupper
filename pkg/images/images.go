4721289bd46ddef7630a9ff9fee9f7ea57496a41
Noe Luaces <nluaces@redhat.com>
2023-05-15 16:07:06 +0200

    update for 1.4.0-rc2

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -2,10 +2,10 @@ package images
 
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
-	RouterImageName            string = "skupper-router:2.4.0-rc1"
-	ServiceControllerImageName string = "service-controller:1.4.0-rc1"
-	ConfigSyncImageName        string = "config-sync:1.4.0-rc1"
-	FlowCollectorImageName     string = "flow-collector:1.4.0-rc1"
+	RouterImageName            string = "skupper-router:2.4.0-rc2"
+	ServiceControllerImageName string = "service-controller:1.4.0-rc2"
+	ConfigSyncImageName        string = "config-sync:1.4.0-rc2"
+	FlowCollectorImageName     string = "flow-collector:1.4.0-rc2"
 	PrometheusImageRegistry    string = "quay.io/prometheus"
 	PrometheusServerImageName  string = "prometheus:v2.42.0"
 )
