f6a7024172d8979715845d7dfd743c7252bd6ee0
Gordon Sim <gsim@redhat.com>
2023-08-29 16:35:58 +0100

    update versions for a 1.2.x respin

--- client/images.go
+++ client/images.go
@@ -3,6 +3,6 @@ package client
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
 	RouterImageName            string = "skupper-router:2.2.1"
-	ServiceControllerImageName string = "service-controller:1.2.7"
-	ConfigSyncImageName        string = "config-sync:1.2.7"
+	ServiceControllerImageName string = "service-controller:1.2.8"
+	ConfigSyncImageName        string = "config-sync:1.2.8"
 )
