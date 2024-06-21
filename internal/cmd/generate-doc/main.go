ef4577ab0c31a39ee01514ca20799e63692e726b
Christian Kruse <ctkruse99@gmail.com>
2024-06-21 10:17:18 -0700

    move generate-doc output to ./doc/cli (#1515)
    * move generate-doc output to ./doc/cli
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>
    
    * exit on error
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>
    
    * unused
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>
    
    * spelling
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>
    
    ---------
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>

--- internal/cmd/generate-doc/main.go
+++ internal/cmd/generate-doc/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/skupperproject/skupper/internal/cmd/skupper/root"
 	"github.com/skupperproject/skupper/internal/cmd/skupper/utils"
 	"github.com/spf13/cobra/doc"
 )
 
 func main() {
+	path, err := checkArgs(os.Args[1:])
+	if err != nil {
+		fmt.Printf("%s\n\nUsage: generate-doc ./docsoutput\n", err)
+		os.Exit(1)
+	}
+	utils.HandleError(doc.GenMarkdownTree(root.NewSkupperRootCommand(), path))
 
-	utils.HandleError(doc.GenMarkdownTree(root.NewSkupperRootCommand(), "."))
+}
 
+func checkArgs(args []string) (path string, err error) {
+	if len(args) != 1 {
+		return path, fmt.Errorf("expected single argument")
+	}
+	path = args[0]
+	info, err := os.Stat(path)
+	if err != nil {
+		return "", fmt.Errorf("output directory %q does not exist", path)
+	}
+	if !info.IsDir() {
+		return "", fmt.Errorf("%q is not of type directory", path)
+	}
+	return path, nil
 }
