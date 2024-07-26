21d5e5f6062af098c77530f786bbc60a9ad7b334
Christian Kruse <christian@c-kruse.com>
2024-07-26 13:24:55 -0700

    Update config-sync to use collector in pkg/kube/flow
    Replaces the collector responsible for updating the
    skupper-network-status configmap for the implementation in
    pkg/kube/flow.
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>

--- cmd/config-sync/collector.go
+++ cmd/config-sync/collector.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"log/slog"
 	"os"
@@ -11,10 +10,8 @@ import (
 	"github.com/skupperproject/skupper/api/types"
 	internalclient "github.com/skupperproject/skupper/internal/kube/client"
 	"github.com/skupperproject/skupper/pkg/config"
-	"github.com/skupperproject/skupper/pkg/flow"
 	"github.com/skupperproject/skupper/pkg/kube"
 	kubeflow "github.com/skupperproject/skupper/pkg/kube/flow"
-	"github.com/skupperproject/skupper/pkg/qdr"
 	"github.com/skupperproject/skupper/pkg/vanflow"
 	"github.com/skupperproject/skupper/pkg/vanflow/session"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -38,8 +35,7 @@ func updateLockOwner(lockname, namespace string, owner *metav1.OwnerReference, c
 	return err
 }
 
-func siteCollector(stopCh <-chan struct{}, cli *internalclient.KubeClient) {
-	var fc *flow.FlowCollector
+func siteCollector(ctx context.Context, cli *internalclient.KubeClient) {
 	siteData := map[string]string{}
 	platform := config.GetPlatform()
 	if platform != types.PlatformKubernetes {
@@ -51,12 +47,7 @@ func siteCollector(stopCh <-chan struct{}, cli *internalclient.KubeClient) {
 	}
 	owner := kube.GetDeploymentOwnerReference(current)
 
-	const shadowNetworkStatusConfigMapName = "skupper-network-status-shadow"
 	existing, err := kube.NewConfigMap(types.NetworkStatusConfigMapName, &siteData, nil, nil, &owner, cli.Namespace, cli.Kube)
-	if err != nil && existing == nil {
-		log.Fatal("Failed to create site status config map ", err.Error())
-	}
-	existing, err = kube.NewConfigMap(shadowNetworkStatusConfigMapName, &siteData, nil, nil, &owner, cli.Namespace, cli.Kube)
 	if err != nil && existing == nil {
 		log.Fatal("Failed to create site status config map ", err.Error())
 	}
@@ -67,43 +58,12 @@ func siteCollector(stopCh <-chan struct{}, cli *internalclient.KubeClient) {
 	}
 
 	factory := session.NewContainerFactory("amqp://localhost:5672", session.ContainerConfig{ContainerID: "kube-flow-collector"})
-	statusSync := kubeflow.NewStatusSync(factory, nil, cli.Kube.CoreV1().ConfigMaps(cli.Namespace), shadowNetworkStatusConfigMapName)
-	fc = flow.NewFlowCollector(flow.FlowCollectorSpec{
-		Mode:                flow.RecordStatus,
-		Namespace:           cli.Namespace,
-		PromReg:             nil,
-		ConnectionFactory:   qdr.NewConnectionFactory("amqp://localhost:5672", nil),
-		FlowRecordTtl:       time.Minute * 15,
-		NetworkStatusClient: cli.Kube,
-	})
-
-	go primeBeacons(fc, cli)
-	log.Println("COLLECTOR: Starting flow collector")
-	go statusSync.Run(context.Background())
-	fc.Start(stopCh)
+	statusSync := kubeflow.NewStatusSync(factory, nil, cli.Kube.CoreV1().ConfigMaps(cli.Namespace), types.NetworkStatusConfigMapName)
+	go statusSync.Run(ctx)
 
 }
 
-// primeBeacons attempts to guess the router and service-controller vanflow IDs
-// to pass to the flow collector in order to accelerate startup time.
-func primeBeacons(fc *flow.FlowCollector, cli *internalclient.KubeClient) {
-	podname, _ := os.Hostname()
-	var prospectRouterID string
-	if len(podname) >= 5 {
-		prospectRouterID = fmt.Sprintf("%s:0", podname[len(podname)-5:])
-	}
-	var siteID string
-	cm, err := kube.WaitConfigMapCreated(types.SiteConfigMapName, cli.Namespace, cli.Kube, 5*time.Second, 250*time.Millisecond)
-	if err != nil {
-		log.Printf("COLLECTOR: failed to get skupper-site ConfigMap. Proceeding without Site ID. %s\n", err)
-	} else if cm != nil {
-		siteID = string(cm.ObjectMeta.UID)
-	}
-	log.Printf("COLLECTOR: Priming site with expected beacons for '%s' and '%s'\n", prospectRouterID, siteID)
-	fc.PrimeSiteBeacons(siteID, prospectRouterID)
-}
-
-func startFlowController(stopCh <-chan struct{}, cli *internalclient.KubeClient) error {
+func startFlowController(ctx context.Context, cli *internalclient.KubeClient) error {
 	siteId := os.Getenv("SKUPPER_SITE_ID")
 
 	deployment, err := kube.GetDeployment(deploymentName(), cli.Namespace, cli.Kube)
@@ -123,16 +83,10 @@ func startFlowController(stopCh <-chan struct{}, cli *internalclient.KubeClient)
 			Platform:   &platform,
 		},
 	})
-	go informer.Run(stopCh)
+	go informer.Run(ctx.Done())
 	//TODO: should watching nodes be optional or should we attempt to determine if we have permissions first?
 	//kubeflow.WatchNodes(controller, cli.Namespace, flowController.UpdateHost)
-	ctx, cancel := context.WithCancel(context.Background())
 	go func() {
-		<-stopCh
-		cancel()
-	}()
-	go func() {
-		defer cancel()
 		fc.Run(ctx)
 		if ctx.Err() == nil {
 			slog.Error("kube flow controller unexpectedly quit")
@@ -142,8 +96,11 @@ func startFlowController(stopCh <-chan struct{}, cli *internalclient.KubeClient)
 }
 
 func runLeaderElection(lock *resourcelock.ConfigMapLock, ctx context.Context, id string, cli *internalclient.KubeClient) {
+	var (
+		leaderCtx context.Context
+		cancel    context.CancelFunc
+	)
 	begin := time.Now()
-	var stopCh chan struct{}
 	podname, _ := os.Hostname()
 	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
 		Lock:            lock,
@@ -154,15 +111,15 @@ func runLeaderElection(lock *resourcelock.ConfigMapLock, ctx context.Context, id
 		Callbacks: leaderelection.LeaderCallbacks{
 			OnStartedLeading: func(c context.Context) {
 				log.Printf("COLLECTOR: Leader %s starting site collection after %s\n", podname, time.Since(begin))
-				stopCh = make(chan struct{})
-				siteCollector(stopCh, cli)
-				if err := startFlowController(stopCh, cli); err != nil {
+				leaderCtx, cancel = context.WithCancel(ctx)
+				siteCollector(leaderCtx, cli)
+				if err := startFlowController(leaderCtx, cli); err != nil {
 					log.Printf("COLLECTOR: Failed to start controller for emitting site events: %s", err)
 				}
 			},
 			OnStoppedLeading: func() {
 				// No longer the leader, transition to inactive
-				close(stopCh)
+				cancel()
 			},
 			OnNewLeader: func(current_id string) {
 				if current_id == id {
