3bfc0889ac2130c25334286313a3fb69692ecaf0
Fernando Giorgetti <fgiorget@redhat.com>
2024-08-06 15:08:43 -0300

    Discovering router access port when not producing a bundle

--- pkg/nonkube/common/site_state_renderer_common.go
+++ pkg/nonkube/common/site_state_renderer_common.go
@@ -5,6 +5,7 @@ import (
 	"maps"
 
 	"github.com/skupperproject/skupper/pkg/nonkube/apis"
+	"github.com/skupperproject/skupper/pkg/utils"
 )
 
 func CopySiteState(siteState *apis.SiteState) *apis.SiteState {
@@ -27,7 +28,15 @@ func CopySiteState(siteState *apis.SiteState) *apis.SiteState {
 func CreateRouterAccess(siteState *apis.SiteState) error {
 	if !siteState.HasRouterAccess() {
 		name := fmt.Sprintf("skupper-local")
-		siteState.CreateRouterAccess(name, 5671)
+		var port = 5671
+		var err error
+		if !siteState.IsBundle() {
+			port, err = utils.TcpPortNextFree(port)
+			if err != nil {
+				return err
+			}
+		}
+		siteState.CreateRouterAccess(name, port)
 	}
 	return nil
 }
