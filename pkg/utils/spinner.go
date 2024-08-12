529f9252ee3a13eb1769ac94d38473f9f042a69f
Noe Luaces <nluaces@redhat.com>
2024-08-12 22:34:30 +0200

    defer spinner stop

--- pkg/utils/spinner.go
+++ pkg/utils/spinner.go
@@ -8,14 +8,13 @@ import (
 func NewSpinner(message string, maxRetries int, function func() error) error {
 
 	spin := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+	defer spin.Stop()
 	spin.Prefix = message
 	spin.FinalMSG = message + "\n"
 
 	spin.Start()
 
 	err := RetryError(time.Second, maxRetries, function)
-
-	spin.Stop()
 
 	if err != nil {
 		return err
