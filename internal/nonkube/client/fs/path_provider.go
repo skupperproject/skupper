c97bcfd8fbe7bc28d8e903ac4cb1146e6b07cdf1
Noe Luaces <nluaces@redhat.com>
2024-09-12 15:22:50 +0200

    modify paths for nonkube cli commands

--- internal/nonkube/client/fs/path_provider.go
+++ internal/nonkube/client/fs/path_provider.go
@@ -7,7 +7,7 @@ type PathProvider struct {
 }
 
 func (p *PathProvider) getDefaultNamespace() string {
-	return ".local/share/skupper/default/prepared/state"
+	return ".local/share/skupper/default/sources"
 }
 
 func (p *PathProvider) GetNamespace() string {
@@ -15,5 +15,5 @@ func (p *PathProvider) GetNamespace() string {
 	if p.Namespace == "" {
 		return p.getDefaultNamespace()
 	}
-	return fmt.Sprintf(".local/share/skupper/%s/prepared/state", p.Namespace)
+	return fmt.Sprintf(".local/share/skupper/%s/sources", p.Namespace)
 }
