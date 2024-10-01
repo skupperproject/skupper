92ed34c6087e1c8cef33f97807c0c1f8abee592c
lynnemorrison <lynne.morrison@ibm.com>
2024-10-01 13:03:47 -0400

    Update link cost when receiving a flow record

--- pkg/flow/flow_mem_driver.go
+++ pkg/flow/flow_mem_driver.go
@@ -811,6 +811,11 @@ func (fc *FlowCollector) updateRecord(record interface{}) error {
 				if link.EndTime > 0 {
 					current.EndTime = link.EndTime
 					fc.deleteRecord(current)
+				} else {
+					if current.LinkCost == nil && link.LinkCost != nil {
+						current.LinkCost = link.LinkCost
+						fc.addRecord(current)
+					}
 				}
 			}
 			fc.updateLastHeard(link.Source)
