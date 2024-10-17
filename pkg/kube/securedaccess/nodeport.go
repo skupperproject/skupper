f08d93163334913c0926f9fe8b174e4c8189907e
Gordon Sim <gsim@redhat.com>
2024-10-17 13:04:20 +0100

    only return nodeport endpoints when port has been assigned

--- pkg/kube/securedaccess/nodeport.go
+++ pkg/kube/securedaccess/nodeport.go
@@ -23,11 +23,13 @@ func newNodeportAccess(m *SecuredAccessManager, clusterHost string) AccessType {
 func (o *NodeportAccessType) RealiseAndResolve(access *skupperv1alpha1.SecuredAccess, svc *corev1.Service) ([]skupperv1alpha1.Endpoint, error) {
 	var endpoints []skupperv1alpha1.Endpoint
 	for _, p := range svc.Spec.Ports {
-		endpoints = append(endpoints, skupperv1alpha1.Endpoint{
-			Name: p.Name,
-			Host: o.clusterHost,
-			Port: strconv.Itoa(int(p.NodePort)),
-		})
+		if p.NodePort != 0 {
+			endpoints = append(endpoints, skupperv1alpha1.Endpoint{
+				Name: p.Name,
+				Host: o.clusterHost,
+				Port: strconv.Itoa(int(p.NodePort)),
+			})
+		}
 	}
 	return endpoints, nil
 }
