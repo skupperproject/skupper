9e18c0d11acf6e291153f36bfa605a21de81892c
Christian Kruse <christian@c-kruse.com>
2024-11-07 08:57:34 -0800

    Updates container build tooling (#1767)
    * Updates container build tooling
    
    * Includes labels on container images suggested in OCI spec
    * Container image executables built with without CGO for consistency
    * All executables built with ldflags "-s -w" by default
    * Make targets updated
      * Uses pattern rules for container image targets
      * Uses variables REGISTRY+IMAGE_TAG instead of individual per-image
        variables like CONTROLLER_IMAGE.
      * `docker-build` now a plain-ole' docker build for the host platform -
        mostly for development use
      * `multiarch-oci` now does multiarch builds with output type of
        oci-archive (tar files in ./oci-archives).
      * `push-multiarch-oci` uses skopeo to push oci archives to registry
      * `(docker|podman)-load-oci` conveneince method to load images from
        multiarch-oci archives to local image storage
      * `describe-multiarch-oci` prints fully qualified images repo:tag@sha256
        for the image index and individual platforms
      * cleans up old package+release targets
    * CircleCI config cleanup unused targets and rework jobs for updated
      container build targets.
    
    * fix docker-load-oci
    
    docker image load does not handle oci images correclty without some
    docker-specific metadata. Use podman to do the conversion.
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>
    
    * Include manifest.json in releases
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>
    
    ---------
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>

---
+++ scripts/oci-index-archive-info.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+REGISTRY=${REGISTRY:-quay.io/skupper}
+IMAGE_TAG=${IMAGE_TAG:-"v2-latest"}
+ARCHIVES_PATH=${ARCHIVES_PATH:-./oci-archives}
+
+skopeo::digest (){
+  skopeo inspect oci-archive:"$1" \
+    | jq -r ".Digest"
+}
+
+skopeo::digest::for::arch (){
+  skopeo inspect --raw oci-archive:"$1" \
+    | jq -r \
+	".manifests[] | select(.platform.architecture == \"$2\" and .platform.os == \"linux\") | .digest"
+}
+
+echo "index:"
+for archive in "${ARCHIVES_PATH}"/*; do
+  if [ -f "$archive" ]; then
+    fn=$(basename -- "$archive")
+    imagename="${fn%.*}"
+    digest=$(skopeo::digest "$archive" "$x")
+    if [ -n "$digest" ]; then
+      printf "  - %s/%s:%s@%s\n" "$REGISTRY" "$imagename" "$IMAGE_TAG" "$digest"
+    fi
+  fi
+done
+for x in "$@"; do
+  echo "$x:"
+  for archive in "${ARCHIVES_PATH}"/*; do
+    if [ -f "$archive" ]; then
+      fn=$(basename -- "$archive")
+      imagename="${fn%.*}"
+	  digest=$(skopeo::digest::for::arch "$archive" "$x")
+	  if [ -n "$digest" ]; then
+        printf "  - %s/%s:%s@%s\n" "$REGISTRY" "$imagename" "$IMAGE_TAG" "$digest"
+      fi
+    fi
+  done
+done
+
