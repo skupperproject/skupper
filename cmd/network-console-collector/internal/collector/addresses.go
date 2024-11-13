d7ce80ea05276eae0f63d816106ef900b4a5f59e
Christian Kruse <christian@c-kruse.com>
2024-11-13 14:20:38 -0800

    Collector: Fix Collector Address Manager bugs
    Addresses are now deleted when associated listeners and connectors are
    gone.
    Adds test coverage.
    
    Signed-off-by: Christian Kruse <christian@c-kruse.com>

--- cmd/network-console-collector/internal/collector/addresses.go
+++ cmd/network-console-collector/internal/collector/addresses.go
@@ -11,22 +11,24 @@ import (
 )
 
 type addressManager struct {
-	logger          *slog.Logger
-	stor            store.Interface
-	mu              sync.Mutex
-	idp             idProvider
-	addresses       map[string]struct{}
-	updateAddresses chan AddressRecord
-	source          store.SourceRef
+	logger              *slog.Logger
+	stor                store.Interface
+	mu                  sync.Mutex
+	idp                 idProvider
+	addresses           map[string]struct{}
+	updateAddresses     chan AddressRecord
+	maybePurgeAddresses chan AddressRecord
+	source              store.SourceRef
 }
 
 func newAddressManager(logger *slog.Logger, stor store.Interface) *addressManager {
 	return &addressManager{
-		logger:          logger,
-		stor:            stor,
-		addresses:       make(map[string]struct{}),
-		updateAddresses: make(chan AddressRecord, 16),
-		idp:             newStableIdentityProvider(),
+		logger:              logger,
+		stor:                stor,
+		addresses:           make(map[string]struct{}),
+		updateAddresses:     make(chan AddressRecord, 16),
+		maybePurgeAddresses: make(chan AddressRecord, 16),
+		idp:                 newStableIdentityProvider(),
 		source: store.SourceRef{
 			Version: "0.1",
 			ID:      "self",
@@ -54,56 +56,93 @@ func (m *addressManager) run(ctx context.Context) func() error {
 						slog.String("name", addr.Name),
 					)
 				}()
+			case addr := <-m.maybePurgeAddresses:
+				func() {
+					m.mu.Lock()
+					defer m.mu.Unlock()
+					if _, ok := m.addresses[addr.ID]; !ok {
+						return
+					}
+					addressName := addr.Name
+					entries := m.stor.Index(IndexByAddress, store.Entry{Record: vanflow.ConnectorRecord{Address: &addressName}})
+					if len(entries) > 0 {
+						return
+					}
+					m.stor.Delete(addr.ID)
+					delete(m.addresses, addr.ID)
+					m.logger.Info("Deleted address record",
+						slog.String("id", addr.ID),
+						slog.String("name", addr.Name),
+					)
+				}()
 			}
 		}
 	}
 }
 
 func (m *addressManager) handleChangeEvent(event changeEvent, stor readonly) {
-	if _, ok := event.(deleteEvent); ok {
+	if delEvent, ok := event.(deleteEvent); ok {
+		address, ok := m.addressFromRecord(delEvent.Record)
+		if !ok {
+			return
+		}
+		if !m.hasAddress(address) {
+			return
+		}
+		m.maybePurgeAddresses <- address
 		return
 	}
 	entry, ok := stor.Get(event.ID())
 	if !ok {
 		return
 	}
-	var (
-		address   string
-		startTime time.Time
-		protocol  string = "tcp"
-	)
-	switch r := entry.Record.(type) {
+	address, ok := m.addressFromRecord(entry.Record)
+	if !ok {
+		return // incomplete
+	}
+	if !m.hasAddress(address) {
+		m.updateAddresses <- address
+	}
+}
+
+func (m *addressManager) hasAddress(address AddressRecord) bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	addressID := m.idp.ID("adr", address.Name, address.Protocol)
+	_, ok := m.addresses[addressID]
+	return ok
+}
+
+func (m *addressManager) addressFromRecord(record vanflow.Record) (AddressRecord, bool) {
+	var addr AddressRecord
+	addr.Start = time.Now()
+	switch r := record.(type) {
 	case vanflow.ListenerRecord:
 		if r.Address != nil {
-			address = *r.Address
+			addr.Name = *r.Address
 		}
 		if r.StartTime != nil {
-			startTime = r.StartTime.Time
+			addr.Start = r.StartTime.Time
 		}
 		if r.Protocol != nil {
-			protocol = *r.Protocol
+			addr.Protocol = *r.Protocol
 		}
 	case vanflow.ConnectorRecord:
 		if r.Address != nil {
-			address = *r.Address
+			addr.Name = *r.Address
 		}
 		if r.StartTime != nil {
-			startTime = r.StartTime.Time
+			addr.Start = r.StartTime.Time
 		}
 		if r.Protocol != nil {
-			protocol = *r.Protocol
+			addr.Protocol = *r.Protocol
 		}
 	default:
+		return addr, false
 	}
-	if address == "" {
-		return
+	if addr.Name == "" || addr.Protocol == "" {
+		return addr, false
 	}
-
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	addressID := m.idp.ID("adr", address, protocol)
-	if _, ok := m.addresses[addressID]; ok {
-		return
-	}
-	m.updateAddresses <- AddressRecord{ID: addressID, Name: address, Protocol: protocol, Start: startTime}
+	addr.ID = m.idp.ID("adr", addr.Name, addr.Protocol)
+	return addr, true
 }
---
+++ cmd/network-console-collector/internal/collector/addresses_test.go
@@ -0,0 +1,152 @@
+package collector
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"sort"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/skupperproject/skupper/pkg/vanflow"
+	"github.com/skupperproject/skupper/pkg/vanflow/store"
+	"gotest.tools/poll"
+)
+
+func TestAddressManager(t *testing.T) {
+	tlog := slog.New(slog.NewTextHandler(io.Discard, nil))
+
+	type testPhase struct {
+		Input             []vanflow.Record
+		ExpectedAddresses []vanflow.Record
+	}
+	testCases := []struct {
+		Name   string
+		Phases []testPhase
+	}{
+		{
+			Name: "simple",
+			Phases: []testPhase{
+				{
+					Input: []vanflow.Record{ // incomplete
+						vanflow.ConnectorRecord{
+							BaseRecord: vanflow.NewBase("c1"), Address: nil,
+						},
+					},
+					ExpectedAddresses: []vanflow.Record{}, //none
+				}, {
+					Input: []vanflow.Record{ // now complete
+						vanflow.ConnectorRecord{
+							BaseRecord: vanflow.NewBase("c1"),
+							Address:    ptrTo("bingo"), Protocol: ptrTo("tcp"),
+						},
+						vanflow.ConnectorRecord{ // multiple
+							BaseRecord: vanflow.NewBase("c2"),
+							Address:    ptrTo("bingo"), Protocol: ptrTo("tcp"),
+						},
+					},
+					ExpectedAddresses: []vanflow.Record{
+						AddressRecord{Name: "bingo", Protocol: "tcp"},
+					},
+				}, {
+					Input:             []vanflow.Record{}, // empty
+					ExpectedAddresses: []vanflow.Record{}, // empty
+				},
+			},
+		},
+		{
+			Name: "listeners",
+			Phases: []testPhase{
+				{
+					Input: []vanflow.Record{ // now complete
+						vanflow.ListenerRecord{
+							BaseRecord: vanflow.NewBase("l1"),
+							Address:    ptrTo("bingo"), Protocol: ptrTo("tcp"),
+						},
+						vanflow.ListenerRecord{
+							BaseRecord: vanflow.NewBase("l2"),
+							Address:    ptrTo("bingo"), Protocol: ptrTo("yodel"),
+						},
+						vanflow.ListenerRecord{
+							BaseRecord: vanflow.NewBase("l3"),
+							Address:    ptrTo("poker"), Protocol: ptrTo("tcp"),
+						},
+						vanflow.ListenerRecord{
+							BaseRecord: vanflow.NewBase("l4"),
+							Address:    ptrTo("chess"), Protocol: ptrTo("tcp"),
+						},
+					},
+					ExpectedAddresses: []vanflow.Record{
+						AddressRecord{Name: "bingo", Protocol: "tcp"},
+						AddressRecord{Name: "bingo", Protocol: "yodel"},
+						AddressRecord{Name: "poker", Protocol: "tcp"},
+						AddressRecord{Name: "chess", Protocol: "tcp"},
+					},
+				}, {
+					Input:             []vanflow.Record{}, // empty
+					ExpectedAddresses: []vanflow.Record{}, // empty
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			stor := store.NewSyncMapStore(store.SyncMapStoreConfig{
+				Indexers: RecordIndexers(),
+			})
+
+			testCtx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			manager := newAddressManager(tlog, stor)
+			go manager.run(testCtx)()
+
+			for _, phase := range tc.Phases {
+				stor.Replace(wrapRecords(phase.Input...))
+				for _, e := range stor.List() {
+					manager.handleChangeEvent(addEvent{Record: e.Record}, stor)
+				}
+				ordered := func(addresses []store.Entry) {
+					sort.Slice(addresses, func(i, j int) bool {
+						left := addresses[i].Record.(AddressRecord)
+						right := addresses[j].Record.(AddressRecord)
+						return left.Name+left.Protocol < right.Name+right.Protocol
+					})
+
+				}
+				expected := wrapRecords(phase.ExpectedAddresses...)
+				ordered(expected)
+				poll.WaitOn(t, func(t poll.LogT) poll.Result {
+					addresses := stor.Index(store.TypeIndex, store.Entry{Record: AddressRecord{}})
+					ordered(addresses)
+					diff := cmp.Diff(
+						addresses,
+						expected,
+						cmpopts.IgnoreFields(AddressRecord{}, "ID"),
+						cmpopts.IgnoreFields(AddressRecord{}, "Start"),
+						cmpopts.IgnoreFields(store.Entry{}, "Metadata"),
+					)
+					if diff == "" {
+						return poll.Success()
+					}
+					return poll.Continue("diff: %s", diff)
+				})
+
+				for _, r := range phase.Input {
+					stor.Delete(r.Identity())
+					manager.handleChangeEvent(deleteEvent{Record: r}, stor)
+				}
+			}
+		})
+	}
+
+}
+
+func wrapRecords(records ...vanflow.Record) []store.Entry {
+	entries := make([]store.Entry, len(records))
+	for i := range records {
+		entries[i].Record = records[i]
+	}
+	return entries
+}
