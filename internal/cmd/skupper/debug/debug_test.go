635828e4108a6e60388eac68b1b1426ce989fa5b
lynnemorrison <lynne.morrison@ibm.com>
2024-12-10 14:03:57 -0500

    Add kube debug dump command

--- internal/cmd/skupper/debug/debug_test.go
+++ internal/cmd/skupper/debug/debug_test.go
@@ -1,1 +1,50 @@
 package debug
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/skupperproject/skupper/internal/cmd/skupper/common"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"gotest.tools/v3/assert"
+)
+
+func TestCmdLinkFactory(t *testing.T) {
+
+	type test struct {
+		name                          string
+		expectedFlagsWithDefaultValue map[string]interface{}
+		command                       *cobra.Command
+	}
+
+	testTable := []test{
+		{
+			name:                          "CmdDebugDumpFactory",
+			expectedFlagsWithDefaultValue: map[string]interface{}{},
+			command:                       CmdDebugDumpFactory(common.PlatformKubernetes),
+		},
+	}
+
+	for _, test := range testTable {
+
+		var flagList []string
+		t.Run(test.name, func(t *testing.T) {
+
+			test.command.Flags().VisitAll(func(flag *pflag.Flag) {
+				flagList = append(flagList, flag.Name)
+				assert.Check(t, test.expectedFlagsWithDefaultValue[flag.Name] != nil, fmt.Sprintf("flag %q not expected", flag.Name))
+				assert.Check(t, test.expectedFlagsWithDefaultValue[flag.Name] == flag.DefValue, fmt.Sprintf("default value %q for flag %q not expected", flag.DefValue, flag.Name))
+			})
+
+			assert.Check(t, len(flagList) == len(test.expectedFlagsWithDefaultValue))
+
+			assert.Assert(t, test.command.PreRunE != nil)
+			assert.Assert(t, test.command.Run != nil)
+			assert.Assert(t, test.command.PostRun != nil)
+			assert.Assert(t, test.command.Use != "")
+			assert.Assert(t, test.command.Short != "")
+			assert.Assert(t, test.command.Long != "")
+		})
+	}
+}
