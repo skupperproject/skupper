576e1a7ce4073c2bfb69e870d0828a07e198482e
Noe Luaces <nluaces@redhat.com>
2025-01-15 12:40:00 +0100

    replace main references for v1

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -2,12 +2,12 @@ package images
 
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
-	RouterImageName            string = "skupper-router:2.7.0"
-	ServiceControllerImageName string = "service-controller:main"
-	ControllerPodmanImageName  string = "controller-podman:main"
-	ConfigSyncImageName        string = "config-sync:main"
-	FlowCollectorImageName     string = "flow-collector:main"
-	SiteControllerImageName    string = "site-controller:main"
+	RouterImageName            string = "skupper-router:2.7.3"
+	ServiceControllerImageName string = "service-controller:v1-dev"
+	ControllerPodmanImageName  string = "controller-podman:v1-dev"
+	ConfigSyncImageName        string = "config-sync:v1-dev"
+	FlowCollectorImageName     string = "flow-collector:v1-dev"
+	SiteControllerImageName    string = "site-controller:v1-dev"
 	PrometheusImageRegistry    string = "quay.io/prometheus"
 	PrometheusServerImageName  string = "prometheus:v2.55.1"
 	OauthProxyImageRegistry    string = "quay.io/openshift"
