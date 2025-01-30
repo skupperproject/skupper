4431b9a45c4ebe9ed9000dead01309317e2d24d6
Noe Luaces <nluaces@redhat.com>
2025-01-30 14:33:23 +0100

    validate the name of the helm chart

--- scripts/push-artifacthub-repo.sh
+++ scripts/push-artifacthub-repo.sh
@@ -3,15 +3,21 @@
 # Check if the user has provided a parameter for the name of the helm chart
 if [ -z "$1" ]; then
   echo "Usage: $0 <helm-chart-name>"
+  echo "Parameter must be 'skupper' or 'network-observer'."
   exit 1
 fi
 
+if [[ "$1" != "skupper" && "$1" != "network-observer" ]]; then
+  echo "Invalid parameter: $1"
+  echo "Parameter must be 'skupper' or 'network-observer'."
+  exit 1
+fi
 
 NAME=$1
 
 cat <<EOF >"artifacthub-repo.yml"
 name: "$NAME"
-displayName: "$NAME Helm chart repository"
+displayName: "$NAME helm chart repository"
 url: "skupper.io"
 publisherID: "skupper-community"
 contactEmail: "skupper@googlegroups.com"
