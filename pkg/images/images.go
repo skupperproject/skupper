07375055c327913331f7d219975af43c7cdf6d02
Noe Luaces <nluaces@redhat.com>
2025-04-08 15:10:49 +0200

    update for 1.9.0

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -2,12 +2,12 @@ package images
 
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
-	RouterImageName            string = "skupper-router:2.7.3"
-	ServiceControllerImageName string = "service-controller:v1-dev"
-	ControllerPodmanImageName  string = "controller-podman:v1-dev"
-	ConfigSyncImageName        string = "config-sync:v1-dev"
-	FlowCollectorImageName     string = "flow-collector:v1-dev"
-	SiteControllerImageName    string = "site-controller:v1-dev"
+	RouterImageName            string = "skupper-router:2.7.5"
+	ServiceControllerImageName string = "service-controller:1.9.0"
+	ControllerPodmanImageName  string = "controller-podman:1.9.0"
+	ConfigSyncImageName        string = "config-sync:1.9.0"
+	FlowCollectorImageName     string = "flow-collector:1.9.0"
+	SiteControllerImageName    string = "site-controller:1.9.0"
 	PrometheusImageRegistry    string = "quay.io/prometheus"
 	PrometheusServerImageName  string = "prometheus:v2.55.1"
 	OauthProxyImageRegistry    string = "quay.io/openshift"
