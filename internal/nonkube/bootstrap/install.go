634deabc3b9523f05f8abfef177467634e152975
Noe Luaces <nluaces@redhat.com>
2025-06-04 17:54:31 +0200

    add system-controller container creation/deletion when using the skupper system install/uninstall commands

--- internal/nonkube/bootstrap/install.go
+++ internal/nonkube/bootstrap/install.go
@@ -1,8 +1,28 @@
 package bootstrap
 
 import (
+	"context"
+	"fmt"
+	"github.com/skupperproject/skupper/internal/images"
+	internalclient "github.com/skupperproject/skupper/internal/nonkube/client/compat"
 	"github.com/skupperproject/skupper/internal/nonkube/common"
+	"github.com/skupperproject/skupper/pkg/container"
+	"github.com/skupperproject/skupper/pkg/nonkube/api"
+	"os"
+	"os/exec"
+	"os/user"
+	"strconv"
+	"strings"
 )
+
+type ControllerConfig struct {
+	containerEngine          string
+	containerEndpointDefault string
+	username                 string
+	hostDataHome             string
+	xdgDataHome              string
+	containerEndpoint        string
+}
 
 func Install(platform string) error {
 
@@ -16,5 +36,179 @@ func Install(platform string) error {
 		return err
 	}
 
+	fmt.Printf("Enabled %s socket \n", platform)
+
+	config, err := configEnvVariables(platform)
+	if err != nil {
+		return err
+	}
+
+	cli, err := internalclient.NewCompatClient(config.containerEndpoint, "")
+	if err != nil {
+		return fmt.Errorf("failed to create container client: %v", err)
+	}
+
+	err = cli.ImagePull(context.TODO(), images.GetSystemControllerImageName())
+	if err != nil {
+		return fmt.Errorf("failed to pull system-controller image: %v", err)
+	}
+	fmt.Printf("Pulled system-controller image: %s\n", images.GetSystemControllerImageName())
+
+	env := map[string]string{
+		"CONTAINER_ENDPOINT":  config.containerEndpoint,
+		"SKUPPER_OUTPUT_PATH": config.hostDataHome,
+		"CONTAINER_ENGINE":    config.containerEngine,
+		"XDG_DATA_HOME":       config.xdgDataHome,
+	}
+
+	//To mount a volume as a bind, the host path must be specified in the Name field
+	//instead of the Source field. If the values Name/Destination are empty, volumes will be ignored, not mounted,
+	//and the system-controller container will fail to start.
+
+	mounts := []container.Volume{}
+
+	if strings.HasPrefix(config.containerEndpoint, "unix://") {
+		socketPath := strings.TrimPrefix(config.containerEndpoint, "unix://")
+		mounts = append(mounts, container.Volume{
+			Name:        socketPath,
+			Destination: "/var/run/docker.sock",
+			Mode:        "z",
+			RW:          true,
+		})
+	} else if strings.HasPrefix(config.containerEndpoint, "/") {
+
+		mounts = append(mounts, container.Volume{
+			Name:        config.containerEndpoint,
+			Destination: "/var/run/docker.sock",
+			Mode:        "z",
+			RW:          true,
+		})
+	}
+
+	mounts = append(mounts, container.Volume{
+		Name:        config.hostDataHome,
+		Destination: "/output",
+		Mode:        "z",
+		RW:          true,
+	})
+
+	//This is necessary to start the container with an equivalent to "--security-opt label=disable"
+	annotations := map[string]string{
+		"io.podman.annotations.label": "disable",
+	}
+
+	sysControllerContainer := container.Container{
+		Name:        fmt.Sprintf("%s-skupper-system-controller", config.username),
+		Image:       images.GetSystemControllerImageName(),
+		Env:         env,
+		Mounts:      mounts,
+		Annotations: annotations,
+	}
+
+	err = cli.ContainerCreate(&sysControllerContainer)
+	if err != nil {
+		return fmt.Errorf("failed to create system-controller container: %v", err)
+	}
+	err = cli.ContainerStart(sysControllerContainer.Name)
+	if err != nil {
+		return fmt.Errorf("failed to start system-controller container: %v", err)
+	}
+
 	return nil
+}
+
+func configEnvVariables(platform string) (*ControllerConfig, error) {
+
+	controllerConfig := ControllerConfig{}
+	var err error
+
+	// Set CONTAINER_ENGINE
+	switch platform {
+	case "docker":
+		_, err = exec.LookPath("docker")
+		if err != nil {
+			return nil, fmt.Errorf("docker not found")
+		}
+
+	default:
+		_, err = exec.LookPath("podman")
+		if err != nil {
+			return nil, fmt.Errorf("podman not found")
+		}
+
+	}
+	err = os.Setenv("CONTAINER_ENGINE", platform)
+	if err != nil {
+		return nil, err
+	}
+
+	controllerConfig.containerEngine = platform
+
+	// Get current username
+	currentUser, err := user.Current()
+	if err != nil {
+		return nil, fmt.Errorf(fmt.Sprintf("Failed to get current user: %v", err))
+	}
+	uid := currentUser.Uid
+	controllerConfig.username = currentUser.Username
+
+	xdgRuntimeDir := os.Getenv("XDG_RUNTIME_DIR")
+	if xdgRuntimeDir == "" {
+		xdgRuntimeDir = fmt.Sprintf("/run/user/%s", uid)
+	}
+
+	containerEndpointDefault := fmt.Sprintf("unix://%s/podman/podman.sock", xdgRuntimeDir)
+
+	if platform == "docker" {
+		containerEndpointDefault = "unix:///run/docker.sock"
+	}
+
+	uidInt, _ := strconv.Atoi(uid)
+	xdgDataHome := "/output"
+	hostDataHome := api.GetHostDataHome()
+	if uidInt == 0 {
+		if platform == "podman" {
+			containerEndpointDefault = "unix:///run/podman/podman.sock"
+		}
+		hostDataHome = "/var/lib/skupper"
+	}
+
+	if err := os.MkdirAll(api.GetDefaultOutputNamespacesPath(), 0755); err != nil {
+		return nil, fmt.Errorf(fmt.Sprintf("Failed to create directory: %v", err))
+	}
+
+	containerEndpoint := os.Getenv("CONTAINER_ENDPOINT")
+	if containerEndpoint == "" {
+		containerEndpoint = containerEndpointDefault
+	}
+
+	err = os.Setenv("CONTAINER_ENDPOINT_DEFAULT", containerEndpointDefault)
+	if err != nil {
+		return nil, err
+	}
+
+	controllerConfig.containerEndpointDefault = containerEndpointDefault
+
+	err = os.Setenv("SKUPPER_OUTPUT_PATH", hostDataHome)
+	if err != nil {
+		return nil, err
+	}
+
+	controllerConfig.hostDataHome = hostDataHome
+
+	err = os.Setenv("XDG_DATA_HOME", xdgDataHome)
+	if err != nil {
+		return nil, err
+	}
+
+	controllerConfig.xdgDataHome = xdgDataHome
+
+	err = os.Setenv("CONTAINER_ENDPOINT", containerEndpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	controllerConfig.containerEndpoint = containerEndpoint
+
+	return &controllerConfig, nil
 }
