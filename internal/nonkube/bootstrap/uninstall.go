346df65d28de507df3d2edaa8a765d529a05ca2f
Noe Luaces <nluaces@redhat.com>
2025-06-04 17:54:31 +0200

    add system-controller container creation/deletion when using the skupper system install/uninstall commands

--- internal/nonkube/bootstrap/uninstall.go
+++ internal/nonkube/bootstrap/uninstall.go
@@ -1,13 +1,43 @@
 package bootstrap
 
 import (
+	"fmt"
+	internalclient "github.com/skupperproject/skupper/internal/nonkube/client/compat"
 	"github.com/skupperproject/skupper/internal/nonkube/common"
 	"github.com/skupperproject/skupper/pkg/nonkube/api"
 	"os"
 	"path"
+	"strings"
 )
 
 func Uninstall(platform string) error {
+
+	cli, err := internalclient.NewCompatClient("", "")
+	if err != nil {
+		return fmt.Errorf("failed to create container client: %v", err)
+	}
+
+	list, err := cli.ContainerList()
+	if err != nil {
+		return err
+	}
+	var systemControllerContainer string
+	for _, container := range list {
+		if strings.HasSuffix(container.Name, "system-controller") {
+			systemControllerContainer = container.Name
+			break
+		}
+	}
+
+	err = cli.ContainerStop(systemControllerContainer)
+	if err != nil {
+		return fmt.Errorf("failed to stop system-controller container: %v", err)
+	}
+
+	err = cli.ContainerRemove(systemControllerContainer)
+	if err != nil {
+		return fmt.Errorf("failed to remove system-controller container: %v", err)
+	}
 
 	systemdGlobal, err := common.NewSystemdGlobal(platform)
 	if err != nil {
@@ -24,7 +54,7 @@ func Uninstall(platform string) error {
 
 func CheckActiveSites() (bool, error) {
 
-	entries, err := os.ReadDir(path.Join(api.GetDataHome(), "namespaces/"))
+	entries, err := os.ReadDir(path.Join(api.GetHostDataHome(), "namespaces/"))
 	if err != nil {
 		return false, err
 	}
