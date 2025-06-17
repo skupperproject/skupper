ce178afe3f499b4dd750abff7d31f852302f4c6c
Noe Luaces <nluaces@redhat.com>
2025-06-17 17:55:22 +0200

    Revert "update for 1.8.6"
    This reverts commit 5fe3e29e9e92a5ab6ab37992e37b8109f6bda839.

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -2,12 +2,12 @@ package images
 
 const (
 	DefaultImageRegistry       string = "quay.io/skupper"
-	RouterImageName            string = "skupper-router:2.7.6"
-	ServiceControllerImageName string = "service-controller:1.8.6"
-	ControllerPodmanImageName  string = "controller-podman:1.8.6"
-	ConfigSyncImageName        string = "config-sync:1.8.6"
-	FlowCollectorImageName     string = "flow-collector:1.8.6"
-	SiteControllerImageName    string = "site-controller:1.8.6"
+	RouterImageName            string = "skupper-router:2.7.5"
+	ServiceControllerImageName string = "service-controller:1.8.5"
+	ControllerPodmanImageName  string = "controller-podman:1.8.5"
+	ConfigSyncImageName        string = "config-sync:1.8.5"
+	FlowCollectorImageName     string = "flow-collector:1.8.5"
+	SiteControllerImageName    string = "site-controller:1.8.5"
 	PrometheusImageRegistry    string = "quay.io/prometheus"
 	PrometheusServerImageName  string = "prometheus:v2.55.1"
 	OauthProxyImageRegistry    string = "quay.io/openshift"
