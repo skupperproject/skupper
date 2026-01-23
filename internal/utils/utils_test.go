f1f69a7070686ca72e4d4b3b831abe3f1fb9e6fe
JPadovano1483 <james.padovano@ibm.com>
2026-01-23 15:17:20 -0500

    cleaning dead code and adding more test coverage

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
-	"gotest.tools/v3/assert"
 	"reflect"
+	"strings"
 	"testing"
+
+	"gotest.tools/v3/assert"
 )
 
 func TestStringifySelector(t *testing.T) {
@@ -27,6 +29,21 @@ func TestStringifySelector(t *testing.T) {
 			assert.Assert(t, reflect.DeepEqual(expectedResMap, actualResMap))
 		})
 	}
+}
+
+// LabelToMap expects label string to be a comma separated
+// list of key and value pairs delimited by equals.
+func LabelToMap(label string) map[string]string {
+	m := map[string]string{}
+	labels := strings.Split(label, ",")
+	for _, l := range labels {
+		if !strings.Contains(l, "=") {
+			continue
+		}
+		entry := strings.Split(l, "=")
+		m[entry[0]] = entry[1]
+	}
+	return m
 }
 
 func TestSliceEquals(t *testing.T) {
@@ -54,26 +71,28 @@ func TestSliceEquals(t *testing.T) {
 	}
 }
 
-func TestGetOrDefault(t *testing.T) {
-	type test struct {
-		name         string
-		value        string
-		defaultValue string
-		result       string
-	}
-
-	testTable := []test{
-		{name: "empty string", value: "", defaultValue: "default-value", result: "default-value"},
-		{name: "valid value", value: "provided-value", defaultValue: "default-value", result: "provided-value"},
-		{name: "both empty", value: "", defaultValue: "", result: ""},
+func TestDefaultStr(t *testing.T) {
+	testTable := []struct {
+		name 	 string
+		values []string
+		result string
+	}{
+		{name: "four non-empty", values: []string{"foo", "bar", "hello", "world"}, result: "foo"},
+		{name: "leading empty strings", values: []string{"", "", "test", "1"}, result: "test"},
+		{name: "single non-empty string", values: []string{"single"}, result: "single"},
+		{name: "empty at end, non-empty at start", values: []string{"first", "", "", ""}, result: "first"},
+		{name: "spaces are not empty", values: []string{"", " ", "test"}, result: " "},
+		{name: "two strings, first empty", values: []string{"", "second"}, result: "second"},
+		{name: "two strings, both non-empty", values: []string{"first", "second"}, result: "first"},
+		{name: "all empty strings", values: []string{"", "", ""}, result: ""},
+		{name: "single empty string", values: []string{""}, result: ""},
 	}
 
 	for _, test := range testTable {
-		t.Run(test.name, func(t *testing.T) {
-
+		t.Run(test.name, func(t * testing.T) {
 			expectedResult := test.result
-			actualResult := GetOrDefault(test.value, test.defaultValue)
-			assert.Assert(t, reflect.DeepEqual(actualResult, expectedResult))
+			actualResult := DefaultStr(test.values...)
+			assert.Equal(t, expectedResult, actualResult)
 		})
 	}
 }
