1f3766ef225512dda33c7a290e1091deb1a351b9
Gabor Dozsa <gabor.dozsa@ibm.com>
2026-03-12 16:05:13 +0100

    Add weighted address strategy for multi-key listeners (#2394)
    Signed-off-by: Gabor Dozsa <gabor.dozsa@ibm.com>

--- internal/site/bindings.go
+++ internal/site/bindings.go
@@ -196,6 +196,17 @@ func (b *Bindings) updateMultiKeyListener(mkl *skupperv2alpha1.MultiKeyListener)
 	if ok && reflect.DeepEqual(existing.Spec, mkl.Spec) {
 		return nil
 	}
+
+	if mkl.Spec.Strategy.Weighted != nil {
+		// update weight values in status if they changed in the spec
+		for k, w := range mkl.Status.Strategy.Weighted.RoutingKeysReachable {
+			if ws, ok := mkl.Spec.Strategy.Weighted.RoutingKeys[k]; ok {
+				if w != ws {
+					mkl.Status.Strategy.Weighted.RoutingKeysReachable[k] = ws
+				}
+			}
+		}
+	}
 	return b
 }
 
